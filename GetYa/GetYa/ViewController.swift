3967f21e08604f1924d3560749b2bb89ff596666
NamSeok-Bae <qoskatjr2@gmail.com>
2023-07-31 15:09:10 +0900

    add: 프로젝트 생성

---
+++ GetYa/GetYa/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GetYa
+//
+//  Created by 배남석 on 2023/07/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GetYa/GetYaUITests/GetYaUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  GetYaUITestsLaunchTests.swift
+//  GetYaUITests
+//
+//  Created by 배남석 on 2023/07/31.
+//
+
+import XCTest
+
+final class GetYaUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
