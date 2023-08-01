76bbe3f27c35c610152b101e73db7a9148afc7fd
choiseona <tjsdk617@naver.com>
2023-08-01 17:40:10 +0900

    chore: 라우터 설정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,17 @@
+import { Route, Routes } from "react-router-dom";
 import Header from "./Components/Common/Header/Header";
 import LandingPage from "./Pages/LandingPage";
+import RecommendQuestionPage from "./Pages/RecommendQuestionPage";
 
 function App() {
   return (
-    <>
+    <div>
       <Header />
-      <LandingPage />
-    </>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/recommendQuestion" element={<RecommendQuestionPage />} />
+      </Routes>
+    </div>
   );
 }
 
---
+++ frontend/src/Pages/RecommendQuestionPage.tsx
@@ -0,0 +1,5 @@
+function RecommendQuestionPage() {
+  return <div className="mt-[92px]">RecommendQuestionPage</div>;
+}
+
+export default RecommendQuestionPage;
