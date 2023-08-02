8844bfa545fb6a26db836f7be4338df324782041
NamSeok-Bae <qoskatjr2@gmail.com>
2023-08-02 15:24:12 +0900

    add: CommonPageControl 생성

---
+++ GetYa/GetYa/Presentation/Common/View/CommonPageControl.swift
@@ -0,0 +1,25 @@
+//
+//  CommonPageControl.swift
+//  GetYa
+//
+//  Created by 배남석 on 2023/08/02.
+//
+
+import UIKit
+
+class CommonPageControl: UIPageControl {
+    // MARK: - UI Properties
+    
+    // MARK: - Properties
+    
+    // MARK: - LifeCycles
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    // MARK: - Functions
+}
