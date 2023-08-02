d2956b3dafca553269e256e7336907ceafa6f394
NamSeok-Bae <qoskatjr2@gmail.com>
2023-08-02 15:04:07 +0900

    chore: CommonButton에서 fileprivate UIColor Extension 위치 변경

--- GetYa/GetYa/Presentation/Common/View/CommonButton.swift
+++ GetYa/GetYa/Presentation/Common/View/CommonButton.swift
@@ -93,22 +93,3 @@ final class CommonButton: UIButton {
         self.layer.borderColor = wideButtonBackgroundColorType.layerBorderColor
     }
 }
-
-// TODO: Color Set 만들기
-fileprivate extension UIColor {
-    convenience init(red: Int, green: Int, blue: Int) {
-        self.init(red: CGFloat(red) / 255.0,
-                  green: CGFloat(green) / 255.0,
-                  blue: CGFloat(blue) / 255.0,
-                  alpha: 1.0)
-    }
-    
-    convenience init(hexString: String) {
-        let arr = Array(hexString)
-        var rgb: [Int] = []
-        for index in stride(from: 0, to: 5, by: 2) {
-            rgb.append(Int(String(arr[index...(index+1)]), radix: 16)!)
-        }
-        self.init(red: rgb[0], green: rgb[1], blue: rgb[2])
-    }
-}
