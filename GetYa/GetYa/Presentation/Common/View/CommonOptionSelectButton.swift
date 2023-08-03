e4e0b1c9b8714751ea9034a08b12e37a14e6e7f3
NamSeok-Bae <qoskatjr2@gmail.com>
2023-08-03 14:09:56 +0900

    add: CommonOptionSelectButton 생성 및 구현

--- GetYa/GetYa/Presentation/Common/View/CommonOptionSelectButton.swift
+++ GetYa/GetYa/Presentation/Common/View/CommonOptionSelectButton.swift
@@ -11,8 +11,38 @@ class CommonOptionSelectButton: UIButton {
     // MARK: - UI Properties
     
     // MARK: - Properties
+    override var isSelected: Bool {
+        didSet {
+            self.layer.backgroundColor = isSelected ? UIColor(hexString: "00428E").cgColor : UIColor.white.cgColor
+        }
+    }
     
     // MARK: - LifeCycles
+    convenience init() {
+        self.init(frame: .zero)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        configureUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        configureUI()
+    }
     
     // MARK: - Functions
+    private func configureUI() {
+        self.translatesAutoresizingMaskIntoConstraints = false
+        self.setTitle("선택", for: .normal)
+        self.setImage(UIImage(named: "Blue-Check-Circle"), for: .normal)
+        self.setImage(UIImage(named: "White-Check-Circle"), for: .selected)
+        self.setTitleColor(UIColor(hexString: "00428E"), for: .normal)
+        self.setTitleColor(.white, for: .selected)
+        self.layer.borderWidth = 1
+        self.layer.borderColor = UIColor(hexString: "00428E").cgColor
+        self.layer.cornerRadius = 20
+        self.layer.backgroundColor = UIColor.white.cgColor
+    }
 }
