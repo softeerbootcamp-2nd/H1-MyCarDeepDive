d71d7408fb694cd3eef4c8f122cd40c796b47a2b
NamSeok-Bae <qoskatjr2@gmail.com>
2023-08-03 13:40:44 +0900

    chore: CommonPageControl hidesForSinglePage 속성 추가

--- GetYa/GetYa/Presentation/Common/View/CommonPageControl.swift
+++ GetYa/GetYa/Presentation/Common/View/CommonPageControl.swift
@@ -33,6 +33,7 @@ class CommonPageControl: UIPageControl {
         self.translatesAutoresizingMaskIntoConstraints = false
         self.isUserInteractionEnabled = false
         self.currentPage = 0
+        self.hidesForSinglePage = true
         
         //TODO: Color 변경
         self.currentPageIndicatorTintColor = UIColor(hexString: "2197C9")
