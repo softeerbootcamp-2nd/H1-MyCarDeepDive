d9bcf85a74d4efe1186a06e0975e4220c310d9a9
DaeHee99 <mokday99@gmail.com>
2023-08-04 03:19:02 +0900

    fix: 버튼 스타일 탐색 비용 개선

--- frontend/src/Components/Common/Button/Button.tsx
+++ frontend/src/Components/Common/Button/Button.tsx
@@ -7,26 +7,20 @@ type ButtonProps = {
   onClick: () => void;
 };
 
-function Button({ width, height, type, variant, text, onClick }: ButtonProps) {
-  const setButtonStyle = (): string => {
-    switch (variant) {
-      case 'primary':
-        return 'bg-primary font-body3-medium text-grey-1000';
-      case 'secondary':
-        return 'bg-transparent border border-grey-600 font-body3-medium text-grey-50';
-      case 'white':
-        return 'bg-grey-900 font-body3-medium text-primary';
-      case 'transparent':
-        return 'bg-transparent border border-grey-1000 font-body3-medium text-grey-900';
-      default:
-        return '';
-    }
-  };
+const buttonStyle = {
+  primary: 'bg-primary font-body3-medium text-grey-1000',
+  secondary:
+    'bg-transparent border border-grey-600 font-body3-medium text-grey-50',
+  white: 'bg-grey-900 font-body3-medium text-primary',
+  transparent:
+    'bg-transparent border border-grey-1000 font-body3-medium text-grey-900',
+};
 
+function Button({ width, height, type, variant, text, onClick }: ButtonProps) {
   return (
     <button
       type={type || 'button'}
-      className={`${width} ${height} ${setButtonStyle()} rounded-md`}
+      className={`${width} ${height} ${buttonStyle[variant]} rounded-md`}
       onClick={onClick}
     >
       {text}
