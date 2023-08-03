d202c0783bff1b5341ad2bb0e7237b6354db0356
NamSeok-Bae <qoskatjr2@gmail.com>
2023-08-03 15:33:24 +0900

    chore: HomeViewController 의 UI를 Home ContentView로 옮김

---
+++ GetYa/GetYa/Presentation/Home/HomeContentView.swift
@@ -0,0 +1,205 @@
+//
+//  HomeContentView.swift
+//  GetYa
+//
+//  Created by 배남석 on 2023/08/03.
+//
+
+import UIKit
+import AVKit
+
+class HomeContentView: UIView {
+    // MARK: - UI Properties
+    
+    // TODO: - Font 바꾸기
+    private let customButton = CommonButton(
+        font: .systemFont(
+            ofSize: CGFloat.scaledWidth(value: 16),
+            weight: UIFont.Weight(500)
+        ),
+        wideButtonBackgroundColorType: .black,
+        title: "직접 만들래요"
+    )
+    
+    // TODO: - Font 바꾸기
+    private let recomandButton = CommonButton(
+        font: .systemFont(
+            ofSize: CGFloat.scaledWidth(value: 16),
+            weight: UIFont.Weight(500)
+        ),
+        wideButtonBackgroundColorType: .white,
+        title: "추천받기"
+    )
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        let text = "당신의 라이프스타일에 맞게\n차량을 추천해드려요."
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = text
+        label.font = .systemFont(ofSize: CGFloat.scaledWidth(value: 20), weight: .medium)
+        label.textColor = .white
+        label.numberOfLines = 0
+        let attributeString = NSMutableAttributedString(string: text)
+        let otherFont = UIFont.systemFont(
+            ofSize: CGFloat.scaledWidth(value: 20),
+            weight: UIFont.Weight(500)
+        )
+        attributeString.addAttribute(
+            .font,
+            value: otherFont,
+            range: (text as NSString).range(of: "라이프스타일")
+        )
+        label.attributedText = attributeString
+        label.sizeToFit()
+        
+        return label
+    }()
+    
+    // MARK: - Properties
+    var videoLooper: AVPlayerLooper!
+    var playerLayer: AVPlayerLayer!
+    
+    // MARK: - LifeCycles
+    convenience init() {
+        self.init(frame: .zero)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+        configureUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupViews()
+        configureUI()
+    }
+    
+    // MARK: - Functions
+    private func setupViews() {
+        setupAVPlayerLayer()
+        [
+            customButton,
+            recomandButton,
+            titleLabel
+        ].forEach {
+            addSubview($0)
+        }
+    }
+    
+    private func setupAVPlayerLayer() {
+        guard let path = Bundle.main.url(forResource: "Home_mp4", withExtension: "mp4") else {
+            return
+        }
+        let asset = AVAsset(url: path)
+        let item = AVPlayerItem(asset: asset)
+        let player = AVQueuePlayer(playerItem: item)
+        playerLayer = AVPlayerLayer(player: player)
+        videoLooper = AVPlayerLooper(player: player, templateItem: item)
+        layer.addSublayer(playerLayer)
+        player.play()
+    }
+    
+    private func configureUI() {
+        configureRecomandWideButton()
+        configureCustomWideButton()
+        configureTitleLabel()
+        configureAVPlayerLayer()
+    }
+    
+    private func configureCustomWideButton() {
+        NSLayoutConstraint.activate([
+            customButton.bottomAnchor.constraint(
+                equalTo: recomandButton.topAnchor,
+                constant: -CGFloat.scaledHeight(value: 8)
+            ),
+            customButton.leadingAnchor.constraint(
+                equalTo: self.leadingAnchor,
+                constant: CGFloat.scaledWidth(value: 17)
+            ),
+            customButton.trailingAnchor.constraint(
+                equalTo: self.trailingAnchor,
+                constant: -CGFloat.scaledWidth(value: 17)
+            ),
+            customButton.heightAnchor.constraint(equalToConstant: CGFloat.scaledHeight(value: 52))
+        ])
+    }
+    
+    private func configureRecomandWideButton() {
+        NSLayoutConstraint.activate([
+            recomandButton.bottomAnchor.constraint(
+                equalTo: self.bottomAnchor
+            ),
+            recomandButton.leadingAnchor.constraint(
+                equalTo: self.leadingAnchor,
+                constant: CGFloat.scaledWidth(value: 17)
+            ),
+            recomandButton.trailingAnchor.constraint(
+                equalTo: self.trailingAnchor,
+                constant: -CGFloat.scaledWidth(value: 17)
+            ),
+            recomandButton.heightAnchor.constraint(equalToConstant: CGFloat.scaledHeight(value: 52))
+        ])
+    }
+    
+    private func configureTitleLabel() {
+        NSLayoutConstraint.activate([
+            titleLabel.leadingAnchor.constraint(
+                equalTo: self.leadingAnchor,
+                constant: 17
+            ),
+            titleLabel.topAnchor.constraint(
+                equalTo: self.safeAreaLayoutGuide.topAnchor,
+                constant: 42
+            )
+        ])
+    }
+    
+    private func configureAVPlayerLayer() {
+        let centerX = self.bounds.maxX / 2
+        let centerY = self.bounds.maxY / 2
+        
+        playerLayer.frame = CGRect(
+            x: centerX - 242,
+            y: centerY - 158,
+            width: CGFloat.scaledWidth(value: 484),
+            height: CGFloat.scaledHeight(value: 316)
+        )
+        
+        let view = UIView(frame: playerLayer.bounds)
+        
+        let colors: [CGColor] = [
+            UIColor.black.cgColor,
+            UIColor.clear.cgColor
+        ]
+        let topGradientLayer = CAGradientLayer()
+        topGradientLayer.colors = colors
+        topGradientLayer.frame = CGRect(
+            x: 0,
+            y: 0,
+            width: CGFloat.scaledWidth(value: 484),
+            height: CGFloat.scaledHeight(value: 85)
+        )
+        topGradientLayer.startPoint = CGPoint(x: 0.5, y: 0.0)
+        topGradientLayer.endPoint = CGPoint(x: 0.5, y: 1.0)
+        topGradientLayer.locations = [0.0, 1.0]
+        
+        let bottomGradientLayer = CAGradientLayer()
+        bottomGradientLayer.colors = colors
+        bottomGradientLayer.frame = CGRect(
+            x: 0,
+            y: view.bounds.height - CGFloat.scaledHeight(value: 85),
+            width: CGFloat.scaledWidth(value: 484),
+            height: CGFloat.scaledHeight(value: 85)
+        )
+        bottomGradientLayer.startPoint = CGPoint(x: 0.5, y: 1.0)
+        bottomGradientLayer.endPoint = CGPoint(x: 0.5, y: 0.0)
+        bottomGradientLayer.locations = [0.0, 1.0]
+        
+        view.layer.addSublayer(topGradientLayer)
+        view.layer.addSublayer(bottomGradientLayer)
+        
+        playerLayer.addSublayer(view.layer)
+    }
+}
--- GetYa/GetYa/Presentation/Home/HomeViewController.swift
+++ GetYa/GetYa/Presentation/Home/HomeViewController.swift
@@ -12,58 +12,11 @@ class HomeViewController: UIViewController {
 
     // MARK: - UI Properties
     
-    // TODO: - Font 바꾸기
-    private let customButton = CommonButton(
-        font: .systemFont(
-            ofSize: CGFloat.scaledWidth(value: 16),
-            weight: UIFont.Weight(500)
-        ),
-        wideButtonBackgroundColorType: .black,
-        title: "직접 만들래요"
-    )
-    
-    // TODO: - Font 바꾸기
-    private let recomandButton = CommonButton(
-        font: .systemFont(
-            ofSize: CGFloat.scaledWidth(value: 16),
-            weight: UIFont.Weight(500)
-        ),
-        wideButtonBackgroundColorType: .white,
-        title: "추천받기"
-    )
-    
-    private let titleLabel: UILabel = {
-        let label = UILabel()
-        let text = "당신의 라이프스타일에 맞게\n차량을 추천해드려요."
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.text = text
-        label.font = .systemFont(ofSize: CGFloat.scaledWidth(value: 20), weight: .medium)
-        label.textColor = .white
-        label.numberOfLines = 0
-        let attributeString = NSMutableAttributedString(string: text)
-        let otherFont = UIFont.systemFont(
-            ofSize: CGFloat.scaledWidth(value: 20),
-            weight: UIFont.Weight(500)
-        )
-        attributeString.addAttribute(
-            .font,
-            value: otherFont,
-            range: (text as NSString).range(of: "라이프스타일")
-        )
-        label.attributedText = attributeString
-        label.sizeToFit()
-        
-        return label
-    }()
-    
     // MARK: - Properties
-    var videoLooper: AVPlayerLooper!
-    var playerLayer: AVPlayerLayer!
     
     // MARK: - LifeCycles
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupViews()
         configureUI()
     }
     
@@ -71,132 +24,24 @@ class HomeViewController: UIViewController {
         return .lightContent
     }
     
-    // MARK: - Functions
-    private func setupViews() {
-        setupAVPlayerLayer()
-        [
-            customButton,
-            recomandButton,
-            titleLabel
-        ].forEach {
-            view.addSubview($0)
-        }
+    override func viewSafeAreaInsetsDidChange() {
+        setupViews()
     }
     
-    private func setupAVPlayerLayer() {
-        guard let path = Bundle.main.url(forResource: "Home_mp4", withExtension: "mp4") else {
-            return
-        }
-        let asset = AVAsset(url: path)
-        let item = AVPlayerItem(asset: asset)
-        let player = AVQueuePlayer(playerItem: item)
-        playerLayer = AVPlayerLayer(player: player)
-        videoLooper = AVPlayerLooper(player: player, templateItem: item)
-        view.layer.addSublayer(playerLayer)
-        player.play()
+    // MARK: - Functions
+    private func setupViews() {
+        let contentView = HomeContentView(frame: view.safeAreaLayoutGuide.layoutFrame)
+        view.addSubview(contentView)
     }
     
     private func configureUI() {
         view.backgroundColor = .black
         
         configureNavigationBar()
-        configureRecomandWideButton()
-        configureCustomWideButton()
-        configureTitleLabel()
-        configureAVPlayerLayer()
     }
     
     private func configureNavigationBar() {
         let image = UIImage(named: "Logo")
         self.navigationItem.titleView = UIImageView(image: image)
-    }
-    
-    private func configureCustomWideButton() {
-        NSLayoutConstraint.activate([
-            customButton.bottomAnchor.constraint(
-                equalTo: recomandButton.topAnchor,
-                constant: -CGFloat.scaledHeight(value: 8)
-            ),
-            customButton.leadingAnchor.constraint(
-                equalTo: view.leadingAnchor,
-                constant: CGFloat.scaledWidth(value: 17)
-            ),
-            customButton.trailingAnchor.constraint(
-                equalTo: view.trailingAnchor,
-                constant: -CGFloat.scaledWidth(value: 17)
-            ),
-            customButton.heightAnchor.constraint(equalToConstant: CGFloat.scaledHeight(value: 52))
-        ])
-    }
-    
-    private func configureRecomandWideButton() {
-        NSLayoutConstraint.activate([
-            recomandButton.bottomAnchor.constraint(
-                equalTo: view.safeAreaLayoutGuide.bottomAnchor
-            ),
-            recomandButton.leadingAnchor.constraint(
-                equalTo: view.leadingAnchor,
-                constant: CGFloat.scaledWidth(value: 17)
-            ),
-            recomandButton.trailingAnchor.constraint(
-                equalTo: view.trailingAnchor,
-                constant: -CGFloat.scaledWidth(value: 17)
-            ),
-            recomandButton.heightAnchor.constraint(equalToConstant: CGFloat.scaledHeight(value: 52))
-        ])
-    }
-    
-    private func configureTitleLabel() {
-        NSLayoutConstraint.activate([
-            titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 17),
-            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 42)
-        ])
-    }
-    
-    private func configureAVPlayerLayer() {
-        let centerX = view.safeAreaLayoutGuide.layoutFrame.maxX / 2
-        let centerY = view.safeAreaLayoutGuide.layoutFrame.maxY / 2
-        
-        playerLayer.frame = CGRect(
-            x: centerX - 242,
-            y: centerY - 158,
-            width: CGFloat.scaledWidth(value: 484),
-            height: CGFloat.scaledHeight(value: 316)
-        )
-        
-        let view = UIView(frame: playerLayer.bounds)
-        
-        let colors: [CGColor] = [
-            UIColor.black.cgColor,
-            UIColor.clear.cgColor
-        ]
-        let topGradientLayer = CAGradientLayer()
-        topGradientLayer.colors = colors
-        topGradientLayer.frame = CGRect(
-            x: 0,
-            y: 0,
-            width: CGFloat.scaledWidth(value: 484),
-            height: CGFloat.scaledHeight(value: 85)
-        )
-        topGradientLayer.startPoint = CGPoint(x: 0.5, y: 0.0)
-        topGradientLayer.endPoint = CGPoint(x: 0.5, y: 1.0)
-        topGradientLayer.locations = [0.0, 1.0]
-        
-        let bottomGradientLayer = CAGradientLayer()
-        bottomGradientLayer.colors = colors
-        bottomGradientLayer.frame = CGRect(
-            x: 0,
-            y: view.bounds.height - CGFloat.scaledHeight(value: 85),
-            width: CGFloat.scaledWidth(value: 484),
-            height: CGFloat.scaledHeight(value: 85)
-        )
-        bottomGradientLayer.startPoint = CGPoint(x: 0.5, y: 1.0)
-        bottomGradientLayer.endPoint = CGPoint(x: 0.5, y: 0.0)
-        bottomGradientLayer.locations = [0.0, 1.0]
-        
-        view.layer.addSublayer(topGradientLayer)
-        view.layer.addSublayer(bottomGradientLayer)
-        
-        playerLayer.addSublayer(view.layer)
     }
 }
