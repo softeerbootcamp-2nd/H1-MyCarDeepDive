5d902b8bdcdf4e58523651d5059ccd11fad9e780
NamSeok-Bae <qoskatjr2@gmail.com>
2023-08-03 17:47:20 +0900

    add: CommonCheckListStackView, CommonCheckListItemView 생성

---
+++ GetYa/GetYa/Presentation/Common/View/CommonCheckListStackView.swift
@@ -0,0 +1,18 @@
+//
+//  CommonCheckListStackView.swift
+//  GetYa
+//
+//  Created by 배남석 on 2023/08/03.
+//
+
+import UIKit
+
+class CommonCheckListStackView: UIStackView {
+    // MARK: - UI Properties
+    
+    // MARK: - Properties
+    
+    // MARK: - LifeCycles
+    
+    // MARK: - Functions
+}
