d986eb56524abd8ce49b4db81f3a745fdacb493e
NamSeok-Bae <qoskatjr2@gmail.com>
2023-08-04 11:43:48 +0900

    feat: CommonCheckListItemView, CommonCheckListStackView 로직 구현

--- GetYa/GetYa/Presentation/Common/View/CommonCheckListItemView.swift
+++ GetYa/GetYa/Presentation/Common/View/CommonCheckListItemView.swift
@@ -7,45 +7,56 @@
 
 import UIKit
 
+protocol CheckListItemViewDelegate: AnyObject {
+    func tappedView(sender: UITapGestureRecognizer)
+}
+
 class CommonCheckListItemView: UIView {
     // MARK: - UI Properties
     private let imageView = UIImageView(image: UIImage(named: "Blue-Check-Circle"))
-    private let label = CommonCheckListItemLabel()
+    private let label = CommonLabel()
     
     // MARK: - Properties
-    private var isTapped: Bool = false {
+    weak var delegate: CheckListItemViewDelegate?
+    var isTapped: Bool = false {
         didSet {
+            // TODO: Font와 Color 바꿔야함.
             self.layer.borderWidth = isTapped ? 2 : 0
             self.layer.backgroundColor = isTapped ? UIColor.white.cgColor : UIColor(hexString: "F0F0F0").cgColor
             imageView.isHidden = isTapped ? false : true
+            label.font = isTapped ? UIFont.systemFont(ofSize: 16, weight: .bold) : UIFont.systemFont(ofSize: 16, weight: .regular)
+            label.textColor = isTapped ? UIColor(hexString: "00428E") : UIColor(hexString: "777777")
             label.isHighlighted = isTapped
         }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        super.touchesEnded(touches, with: event)
-        isTapped = !isTapped
     }
     
     // MARK: - LifeCycles
     convenience init(text: String) {
         self.init(frame: .zero)
-        configureLabel(text: text)
+        setLabelText(text: text)
     }
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupViews()
         configureUI()
+        setupTapGesture()
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         setupViews()
         configureUI()
+        setupTapGesture()
     }
     
     // MARK: - Functions
+    private func setupTapGesture() {
+        let tapGesture = UITapGestureRecognizer()
+        tapGesture.addTarget(self, action: #selector(tappedView(_:)))
+        self.addGestureRecognizer(tapGesture)
+    }
+    
     private func setupViews() {
         [
             imageView,
@@ -59,22 +70,33 @@ class CommonCheckListItemView: UIView {
         self.translatesAutoresizingMaskIntoConstraints = false
         self.layer.cornerRadius = 6
         
+        // TODO: 색깔 재정의가 필요함.
+        self.layer.borderColor = UIColor(hexString: "00428E").cgColor
+        self.isTapped = false
+        
+        configureLabel()
+        configureImageView()
+    }
+    
+    func setLabelText(text: String) {
+        label.text = text
+        label.layer.borderColor = UIColor(hexString: "00428E").cgColor
+    }
+    
+    private func configureLabel() {
         // TODO: 크기에 관해서 재정의 해줘야함.
         NSLayoutConstraint.activate([
             label.topAnchor.constraint(equalTo: self.topAnchor, constant: 16),
-            label.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: 16),
+            label.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -16),
             label.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 12)
         ])
     }
     
-    private func configureLabel(text: String) {
-        label.isHighlighted = false
-        label.text = text
-    }
-    
-    
     private func configureImageView() {
         // TODO: 크기에 관해서 재정의 해줘야함.
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.isHidden = true
+        
         NSLayoutConstraint.activate([
             imageView.trailingAnchor.constraint(
                 equalTo: self.trailingAnchor,
@@ -84,5 +106,12 @@ class CommonCheckListItemView: UIView {
             imageView.heightAnchor.constraint(equalToConstant: 24),
             imageView.widthAnchor.constraint(equalToConstant: 24)
         ])
+    }
+    
+    // MARK: - Objc Functions
+    @objc private func tappedView(_ sender: UITapGestureRecognizer) {
+        if isTapped { return }
+        isTapped = !isTapped
+        delegate?.tappedView(sender: sender)
     }
 }
--- GetYa/GetYa/Presentation/Common/View/CommonCheckListStackView.swift
+++ GetYa/GetYa/Presentation/Common/View/CommonCheckListStackView.swift
@@ -17,13 +17,55 @@ class CommonCheckListStackView: UIStackView {
         self.init(frame: .zero)
     }
     
+    init(textArray: [String]) {
+        super.init(frame: .zero)
+        configureUI()
+        setupItem(textArray: textArray)
+    }
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
+        configureUI()
     }
     
     required init(coder: NSCoder) {
         super.init(coder: coder)
+        configureUI()
     }
     
     // MARK: - Functions
+    private func removeAllArrangedSubViews() {
+        self.arrangedSubviews.forEach {
+            $0.removeFromSuperview()
+        }
+    }
+    
+    func setupItem(textArray: [String]) {
+        removeAllArrangedSubViews()
+        
+        textArray.forEach {
+            let itemView = CommonCheckListItemView(text: $0)
+            itemView.delegate = self
+            addArrangedSubview(itemView)
+        }
+    }
+    
+    private func configureUI() {
+        self.translatesAutoresizingMaskIntoConstraints = false
+        self.axis = .vertical
+        
+        // TODO: 크기 스케일 해야할까?
+        self.spacing = 12
+    }
+}
+
+// TODO: 고른 선택지에 대한 데이터 저장에 대해 고민해야함.
+extension CommonCheckListStackView: CheckListItemViewDelegate {
+    func tappedView(sender: UITapGestureRecognizer) {
+        arrangedSubviews.map { $0 as? CommonCheckListItemView }.forEach {
+            if $0 != sender.view {
+                $0?.isTapped = false
+            }
+        }
+    }
 }
