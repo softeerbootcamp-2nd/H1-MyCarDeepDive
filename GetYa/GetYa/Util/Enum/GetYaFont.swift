5325b5f21e80fa85be330321b9ef65a076d3c675
NamSeok-Bae <qoskatjr2@gmail.com>
2023-08-05 19:45:25 +0900

    add: GetYaFont regularHead2 Case 추가

--- GetYa/GetYa/Util/Enum/GetYaFont.swift
+++ GetYa/GetYa/Util/Enum/GetYaFont.swift
@@ -12,6 +12,7 @@ enum GetYaFont: CaseIterable {
   case mediumHead2
   case mediumHead3
   case mediumHead4
+  case regularHead2
   case boldBody1
   case mediumBody1
   case boldBody2
@@ -29,20 +30,34 @@ enum GetYaFont: CaseIterable {
     case .boldHead1:
       return 22
     case .mediumHead2:
+        return 20
+    case .regularHead2:
       return 20
     case .mediumHead3:
       return 18
     case .mediumHead4:
       return 16
-    case .boldBody1, .mediumBody1:
+    case .boldBody1:
+        return 20
+    case .mediumBody1:
       return 20
-    case .boldBody2, .mediumBody2, .regularBody2:
+    case .boldBody2:
+        return 18
+    case .mediumBody2:
+        return 18
+    case .regularBody2:
       return 18
-    case .mediumBody3, .regularBody3:
+    case .mediumBody3:
+        return 16
+    case .regularBody3:
       return 16
-    case .mediumBody4, .regularBody4:
+    case .mediumBody4:
+        return 14
+    case .regularBody4:
       return 14
-    case .mediumCaption1, .regularCaption1:
+    case .mediumCaption1:
+        return 12
+    case .regularCaption1:
       return 12
     }
   }
@@ -51,14 +66,35 @@ enum GetYaFont: CaseIterable {
     switch self {
     case .boldHead1:
       return -0.4
-    case .mediumHead2, .mediumHead3, .mediumHead4:
+    case .mediumHead2:
+        return -0.3
+    case .mediumHead3:
+        return -0.3
+    case .mediumHead4:
+        return -0.3
+    case .regularHead2:
       return -0.3
-    case .boldBody1, .mediumBody1, .boldBody2,
-        .mediumBody2, .regularBody2,
-        .mediumBody3, .regularBody3,
-        .mediumBody4, .regularBody4:
+    case .boldBody1:
+        return -0.2
+    case .mediumBody1:
+        return -0.2
+    case .boldBody2:
+        return -0.2
+    case .mediumBody2:
+        return -0.2
+    case .regularBody2:
+        return -0.2
+    case .mediumBody3:
+        return -0.2
+    case .regularBody3:
+        return -0.2
+    case .mediumBody4:
+        return -0.2
+    case .regularBody4:
       return -0.2
-    case .mediumCaption1, .regularCaption1:
+    case .mediumCaption1:
+        return 0.0
+    case .regularCaption1:
       return 0.0
     }
   }
@@ -68,20 +104,34 @@ enum GetYaFont: CaseIterable {
     case .boldHead1:
       return 28
     case .mediumHead2:
+        return 26
+    case .regularHead2:
       return 26
     case .mediumHead3:
       return 24
     case .mediumHead4:
       return 22
-    case .boldBody1, .mediumBody1:
+    case .boldBody1:
+        return 28
+    case .mediumBody1:
       return 28
-    case .boldBody2, .mediumBody2, .regularBody2:
+    case .boldBody2:
+        return 26
+    case .mediumBody2:
+        return 26
+    case .regularBody2:
       return 26
-    case .mediumBody3, .regularBody3:
+    case .mediumBody3:
+        return 24
+    case .regularBody3:
       return 24
-    case .mediumBody4, .regularBody4:
+    case .mediumBody4:
+        return 22
+    case .regularBody4:
       return 22
-    case .mediumCaption1, .regularCaption1:
+    case .mediumCaption1:
+        return 18
+    case .regularCaption1:
       return 18
     }
   }
@@ -90,13 +140,31 @@ enum GetYaFont: CaseIterable {
     switch self {
     case .boldHead1:
       return "HyundaiSansHeadKRBold"
-    case .mediumHead2, .mediumHead3, .mediumHead4:
+    case .mediumHead2:
+        return "HyundaiSansHeadKRMedium"
+    case .mediumHead3:
+        return "HyundaiSansHeadKRMedium"
+    case .mediumHead4:
       return "HyundaiSansHeadKRMedium"
-    case .boldBody1, .boldBody2:
+    case .regularHead2:
+        return "HyundaiSansHeadKRRegular"
+    case .boldBody1:
+        return "HyundaiSansTextKRBold"
+    case .boldBody2:
       return "HyundaiSansTextKRBold"
-    case .mediumBody1, .mediumBody2, .mediumBody3, .mediumBody4:
+    case .mediumBody1:
+        return "HyundaiSansTextKRMedium"
+    case .mediumBody2:
+        return "HyundaiSansTextKRMedium"
+    case .mediumBody3:
+        return "HyundaiSansTextKRMedium"
+    case .mediumBody4:
       return "HyundaiSansTextKRMedium"
-    case .regularBody2, .regularBody3, .regularBody4:
+    case .regularBody2:
+        return "HyundaiSansTextKRRegular"
+    case .regularBody3:
+        return "HyundaiSansTextKRRegular"
+    case .regularBody4:
       return "HyundaiSansTextKRRegular"
     case .mediumCaption1:
       return "HyundaiSansTextKRMedium"
