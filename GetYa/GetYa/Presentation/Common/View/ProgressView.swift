205e33ea4361454518a597ad225b911533eb4162
shcommit <happysh_s2@naver.com>
2023-08-06 22:14:03 +0900

    feat: isOutOfGauge(_:) animate(from:) private func 추가

--- GetYa/GetYa/Presentation/Common/View/ProgressView.swift
+++ GetYa/GetYa/Presentation/Common/View/ProgressView.swift
@@ -13,11 +13,14 @@ final class ProgressView: UIProgressView {
     
     private lazy var minProgressValue: Float = gaugeOneStepAmount
     
+    // MARK: - Properties
+    
     private var progressTotalStep: Int = 0
+    
+    private var animationDuration: TimeInterval = 0.7
     
     private var isWorkingAnimation: Bool = false
     
-    // MARK: - Properties
     private var gaugeOneStepAmount: Float {
         1.0 / Float(progressTotalStep)
     }
@@ -28,9 +31,11 @@ final class ProgressView: UIProgressView {
         progressTotalStep: Int,
         style: UIProgressView.Style,
         progressTintColor: UIColor?,
-        backgroundTintColor: UIColor?
+        backgroundTintColor: UIColor?,
+        animationDuration: TimeInterval
     ) {
         self.progressTotalStep = progressTotalStep
+        self.animationDuration = animationDuration
         super.init(frame: frame)
         progressViewStyle = style
         configureUIColor(
@@ -41,24 +46,28 @@ final class ProgressView: UIProgressView {
     convenience init(
         progressTotalStep: Int,
         progressTintColor: UIColor?,
-        backgroundTintColor: UIColor?
+        backgroundTintColor: UIColor?,
+        animationDuration: TimeInterval
     ) {
         self.init(
             frame: .zero,
             progressTotalStep: progressTotalStep,
             style: .bar,
             progressTintColor: progressTintColor,
-            backgroundTintColor: backgroundTintColor)
+            backgroundTintColor: backgroundTintColor,
+            animationDuration: animationDuration)
         translatesAutoresizingMaskIntoConstraints = false
     }
     
+    // TODO: - 남석이형이 수정한 빨래트로 색 변경.
     convenience init(progressTotalStep: Int) {
         self.init(
             frame: .zero,
             progressTotalStep: progressTotalStep,
             style: .bar,
             progressTintColor: .GetYaPalette.acriveBlue,
-            backgroundTintColor: .GetYaPalette.gray800)
+            backgroundTintColor: .GetYaPalette.gray800,
+            animationDuration: 0.7)
         translatesAutoresizingMaskIntoConstraints = false
     }
     
@@ -80,5 +89,21 @@ extension ProgressView {
     
     func configureStyle(_ style: UIProgressView.Style) {
         progressViewStyle = style
+    }
+}
+
+// MARK: - Private helper
+extension ProgressView {
+    private func isOutOfGauge(_ gauge: Float) -> Bool {
+        !(minProgressValue...maxProgressValue).contains(gauge)
+    }
+    
+    private func animate(from targetProgress: Float) {
+        UIView.animate(
+            withDuration: animationDuration,
+            delay: 0,
+            options: .curveEaseInOut) {
+                self.setProgress(targetProgress, animated: true)
+            }
     }
 }
