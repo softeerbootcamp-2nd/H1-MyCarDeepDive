895594cbdf30d213600287d5df6c92db98de9290
shcommit <happysh_s2@naver.com>
2023-08-06 22:02:07 +0900

    feat: ProgressView 프로퍼티, init함수, convenience init 지정

--- GetYa/GetYa/Presentation/Common/View/ProgressView.swift
+++ GetYa/GetYa/Presentation/Common/View/ProgressView.swift
@@ -5,3 +5,80 @@
 //  Created by 양승현 on 2023/08/06.
 //
 
+import UIKit
+
+final class ProgressView: UIProgressView {
+    // MARK: - Constant
+    private let maxProgressValue: Float = 1.0
+    
+    private lazy var minProgressValue: Float = gaugeOneStepAmount
+    
+    private var progressTotalStep: Int = 0
+    
+    private var isWorkingAnimation: Bool = false
+    
+    // MARK: - Properties
+    private var gaugeOneStepAmount: Float {
+        1.0 / Float(progressTotalStep)
+    }
+    
+    // MARK: - Lifecycles
+    init(
+        frame: CGRect,
+        progressTotalStep: Int,
+        style: UIProgressView.Style,
+        progressTintColor: UIColor?,
+        backgroundTintColor: UIColor?
+    ) {
+        self.progressTotalStep = progressTotalStep
+        super.init(frame: frame)
+        progressViewStyle = style
+        configureUIColor(
+            progressTintColor: progressTintColor,
+            backgroundTintColor: backgroundTintColor)
+    }
+    
+    convenience init(
+        progressTotalStep: Int,
+        progressTintColor: UIColor?,
+        backgroundTintColor: UIColor?
+    ) {
+        self.init(
+            frame: .zero,
+            progressTotalStep: progressTotalStep,
+            style: .bar,
+            progressTintColor: progressTintColor,
+            backgroundTintColor: backgroundTintColor)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    convenience init(progressTotalStep: Int) {
+        self.init(
+            frame: .zero,
+            progressTotalStep: progressTotalStep,
+            style: .bar,
+            progressTintColor: .GetYaPalette.acriveBlue,
+            backgroundTintColor: .GetYaPalette.gray800)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        progressViewStyle = .default
+    }
+}
+
+// MARK: - Helper
+extension ProgressView {
+    func configureUIColor(
+        progressTintColor: UIColor?,
+        backgroundTintColor: UIColor?
+    ) {
+        self.progressTintColor = progressTintColor
+        trackTintColor = backgroundTintColor
+    }
+    
+    func configureStyle(_ style: UIProgressView.Style) {
+        progressViewStyle = style
+    }
+}
