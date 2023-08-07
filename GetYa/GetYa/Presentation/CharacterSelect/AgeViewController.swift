4bb97e43f2c46edd3228144832f081fe78cb9375
NamSeok-Bae <qoskatjr2@gmail.com>
2023-08-07 15:19:56 +0900

    add: AgeViewController, QuestionContentView 생성

---
+++ GetYa/GetYa/Presentation/CharacterSelect/AgeViewController.swift
@@ -0,0 +1,24 @@
+//
+//  AgeViewController.swift
+//  GetYa
+//
+//  Created by 배남석 on 2023/08/07.
+//
+
+import UIKit
+
+class AgeViewController: UIViewController {
+    // MARK: - UI Properties
+    
+    // MARK: - Properties
+    
+    // MARK: - LifeCycles
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    // MARK: - Functions
+
+}
---
+++ GetYa/GetYa/Presentation/CharacterSelect/View/QuestionContentView.swift
@@ -0,0 +1,18 @@
+//
+//  QuestionContentView.swift
+//  GetYa
+//
+//  Created by 배남석 on 2023/08/07.
+//
+
+import UIKit
+
+class QuestionContentView: UIView {
+    // MARK: - UI Properties
+    
+    // MARK: - Properties
+    
+    // MARK: - LifeCycles
+    
+    // MARK: - Functions
+}
