78fcf1e2c850a0e09f8348a4fc6a326caf83e624
NamSeok-Bae <qoskatjr2@gmail.com>
2023-08-07 13:35:38 +0900

    chore: NSMutableAttributedString Extension 텍스트 일부 폰트 적용 구현

--- GetYa/GetYa/Util/Extension/NSMutableAttributedString+.swift
+++ GetYa/GetYa/Util/Extension/NSMutableAttributedString+.swift
@@ -1,5 +1,5 @@
 //
-//  NSMutableAttributedString+.swift
+//  NSMutableAttributedString+HyundaiSansConfigure.swift
 //  GetYa
 //
 //  Created by 양승현 on 2023/08/04.
@@ -8,33 +8,50 @@
 import UIKit
 
 extension NSMutableAttributedString {
-  func configureHyundaiSans(with type: GetYaFont) {
-    guard let font = UIFont(hyundaiSans: type) else {
-      print("DEBUG: Filed to create hyundaiSans font.")
-      return
-    }
-    let attributes = configureHyundaiSansAttributes(
-      with: font,
-      type: type)
-    
-    addAttributes(
-      attributes,
-      range: .init(location: 0, length: string.count))
-  }
-  
-  private func configureHyundaiSansAttributes(
-    with font: UIFont,
-    type: GetYaFont
-  ) -> [NSAttributedString.Key: Any] {
-    let style = NSMutableParagraphStyle().set {
-      $0.maximumLineHeight = type.lineHeight
-      $0.minimumLineHeight = type.lineHeight
+    func configureHyundaiSans(type: GetYaFont) {
+        let font = type.uiFont
+        let attributes = configureHyundaiSansAttributes(
+            with: font,
+            type: type)
+        
+        addAttributes(
+            attributes,
+            range: .init(location: 0, length: string.count))
     }
     
-    return [
-      .kern: type.kern,
-      .font: font,
-      .paragraphStyle: style,
-      .baselineOffset: (type.lineHeight - font.lineHeight) / 4]
-  }
+    func configureHyundaiSans(type: GetYaFont, otherType: GetYaFont, text: String) {
+        let font = type.uiFont
+        let otherFont = otherType.uiFont
+        let attributes = configureHyundaiSansAttributes(
+            with: font,
+            type: type)
+        
+        addAttributes(
+            attributes,
+            range: .init(location: 0, length: string.count))
+        
+        let otherAttributes = configureHyundaiSansAttributes(
+            with: otherFont,
+            type: otherType)
+        
+        addAttributes(
+            otherAttributes,
+            range: (string as NSString).range(of: text))
+    }
+    
+    private func configureHyundaiSansAttributes(
+        with font: UIFont,
+        type: GetYaFont
+    ) -> [NSAttributedString.Key: Any] {
+        let style = NSMutableParagraphStyle().set {
+            $0.maximumLineHeight = type.lineHeight
+            $0.minimumLineHeight = type.lineHeight
+        }
+        
+        return [
+            .kern: type.kern,
+            .font: font,
+            .paragraphStyle: style,
+            .baselineOffset: (type.lineHeight - font.lineHeight) / 4]
+    }
 }
