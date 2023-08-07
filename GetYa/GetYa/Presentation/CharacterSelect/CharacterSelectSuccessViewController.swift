c05b9388f9a26b09be0c703c4f114014f5d37d42
shcommit <happysh_s2@naver.com>
2023-08-07 13:29:58 +0900

    chore: 기본 생성 추가

--- GetYa/GetYa/Presentation/CharacterSelect/CharacterSelectSuccessViewController.swift
+++ GetYa/GetYa/Presentation/CharacterSelect/CharacterSelectSuccessViewController.swift
@@ -5,4 +5,17 @@
 //  Created by 양승현 on 2023/08/07.
 //
 
-import Foundation
+import UIKit
+
+final class CharacterSelectSuccessViewController: UIViewController {
+    // MARK: - Properties
+    
+    // MARK: - Lifecycles
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
