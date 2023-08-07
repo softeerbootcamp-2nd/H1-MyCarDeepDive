22337852ff0c12097d7701eb332af5aec9aa3411
NamSeok-Bae <qoskatjr2@gmail.com>
2023-08-07 13:37:49 +0900

    add: LifeStyleLastCell 생성

---
+++ GetYa/GetYa/Presentation/CharacterSelect/View/LifeStyleLastCell.swift
@@ -0,0 +1,18 @@
+//
+//  LifeStyleLastCell.swift
+//  GetYa
+//
+//  Created by 배남석 on 2023/08/07.
+//
+
+import UIKit
+
+class LifeStyleLastCell: UICollectionViewCell {
+    // MARK: - UI Properties
+    
+    // MARK: - Properties
+    
+    // MARK: - LifeCycles
+    
+    // MARK: - Functions
+}
