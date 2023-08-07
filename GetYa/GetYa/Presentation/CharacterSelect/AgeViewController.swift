06ea9cbebc6fc65cece606881f4232171c927500
NamSeok-Bae <qoskatjr2@gmail.com>
2023-08-07 23:22:37 +0900

    feat: AgeViewController UI 구현

--- GetYa/GetYa/Presentation/CharacterSelect/AgeViewController.swift
+++ GetYa/GetYa/Presentation/CharacterSelect/AgeViewController.swift
@@ -9,16 +9,60 @@ import UIKit
 
 class AgeViewController: UIViewController {
     // MARK: - UI Properties
+    private let contentView = QuestionContentView()
+    private let checkListStackView = CheckListStackView()
     
     // MARK: - Properties
+    private let checkListStackViewLayoutConstant = UILayout(
+        leadingMargin: 16,
+        topMargin: 72,
+        trailingMargin: -16)
+    private let checkListTexts: [String] = [
+        "20대",
+        "30대",
+        "40대",
+        "50대 이상"
+    ]
     
     // MARK: - LifeCycles
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setupViews()
+        configureUI()
     }
     
     // MARK: - Functions
-
+    private func setupViews() {
+        contentView.addSubview(checkListStackView)
+    }
+    
+    private func configureUI() {
+        self.view.backgroundColor = .white
+        contentView.configureDetail(
+            descriptionText: "나이를 알려주세요.",
+            partText: "나이",
+            questionNumber: 1,
+            questionCount: 2,
+            buttonTitle: "다음"
+        )
+        view = contentView
+        configureCheckListStackView()
+    }
+    
+    private func configureCheckListStackView() {
+        checkListStackView.setupItem(textArray: checkListTexts)
+        
+        NSLayoutConstraint.activate([
+            checkListStackView.topAnchor.constraint(
+                equalTo: contentView.descriptionLabel.bottomAnchor,
+                constant: checkListStackViewLayoutConstant.topMargin),
+            checkListStackView.leadingAnchor.constraint(
+                equalTo: view.leadingAnchor,
+                constant: checkListStackViewLayoutConstant.leadingMargin),
+            checkListStackView.trailingAnchor.constraint(
+                equalTo: view.trailingAnchor,
+                constant: checkListStackViewLayoutConstant.trailingMargin)
+        ])
+    }
 }
