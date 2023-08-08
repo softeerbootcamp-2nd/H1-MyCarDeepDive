4e1218f5ef3ab834ae488aa05b51c964204b8e6b
NamSeok-Bae <qoskatjr2@gmail.com>
2023-08-08 13:49:02 +0900

    add: DetailLifeStyleSelectViewController 생성

---
+++ GetYa/GetYa/Presentation/CharacterSelect/DetailLifeStyleSelectViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DetailLifeStyleSelectViewController.swift
+//  GetYa
+//
+//  Created by 배남석 on 2023/08/08.
+//
+
+import UIKit
+
+class DetailLifeStyleSelectViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
