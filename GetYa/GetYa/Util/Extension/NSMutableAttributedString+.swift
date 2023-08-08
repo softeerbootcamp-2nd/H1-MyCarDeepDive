69d7c56a74624ec4f7fdbbc6e294e142167229f1
shcommit <happysh_s2@naver.com>
2023-08-08 14:48:39 +0900

    chore: indent 4로 변경

--- GetYa/GetYa/Util/Extension/NSMutableAttributedString+.swift
+++ GetYa/GetYa/Util/Extension/NSMutableAttributedString+.swift
@@ -8,33 +8,35 @@
 import UIKit
 
 extension NSMutableAttributedString {
-  func configureHyundaiSans(with type: GetYaFont) {
-    guard let font = UIFont(hyundaiSans: type) else {
-      print("DEBUG: Filed to create hyundaiSans font.")
-      return
-    }
-    let attributes = configureHyundaiSansAttributes(
-      with: font,
-      type: type)
-    
-    addAttributes(
-      attributes,
-      range: .init(location: 0, length: string.count))
-  }
-  
-  private func configureHyundaiSansAttributes(
-    with font: UIFont,
-    type: GetYaFont
-  ) -> [NSAttributedString.Key: Any] {
-    let style = NSMutableParagraphStyle().set {
-      $0.maximumLineHeight = type.lineHeight
-      $0.minimumLineHeight = type.lineHeight
+    func configureHyundaiSans(with type: GetYaFont) {
+        guard let font = UIFont(hyundaiSans: type) else {
+            print("DEBUG: Filed to create hyundaiSans font.")
+            return
+        }
+        let attributes = configureHyundaiSansAttributes(
+            with: font,
+            type: type)
+        
+        addAttributes(
+            attributes,
+            range: .init(location: 0, length: string.count))
     }
     
-    return [
-      .kern: type.kern,
-      .font: font,
-      .paragraphStyle: style,
-      .baselineOffset: (type.lineHeight - font.lineHeight) / 4]
-  }
+    
+    
+    private func configureHyundaiSansAttributes(
+        with font: UIFont,
+        type: GetYaFont
+    ) -> [NSAttributedString.Key: Any] {
+        let style = NSMutableParagraphStyle().set {
+            $0.maximumLineHeight = type.lineHeight
+            $0.minimumLineHeight = type.lineHeight
+        }
+        
+        return [
+            .kern: type.kern,
+            .font: font,
+            .paragraphStyle: style,
+            .baselineOffset: (type.lineHeight - font.lineHeight) / 4]
+    }
 }
