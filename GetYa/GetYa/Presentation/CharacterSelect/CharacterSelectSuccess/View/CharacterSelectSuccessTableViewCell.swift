38556aa0e6075df2541ddd036871e2724ce80bdb
shcommit <happysh_s2@naver.com>
2023-08-08 21:22:42 +0900

    feat: Character select success table view cell specific id, init함수 추가

--- GetYa/GetYa/Presentation/CharacterSelect/CharacterSelectSuccess/View/CharacterSelectSuccessTableViewCell.swift
+++ GetYa/GetYa/Presentation/CharacterSelect/CharacterSelectSuccess/View/CharacterSelectSuccessTableViewCell.swift
@@ -5,4 +5,24 @@
 //  Created by 양승현 on 2023/08/08.
 //
 
-import Foundation
+import UIKit
+
+final class CharacterSelectSuccessTableViewCell: UITableViewCell {
+    static let id = String(describing: CharacterSelectSuccessTableViewCell.self)
+    
+    // MARK: - UI properties
+    // MARK: - Properties
+    // MARK: - Lifecycles
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        backgroundColor = .systemPink.withAlphaComponent(0.8)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    // MARK: - Private Functions
+    // MARK: - Functions
+    // MARK: - Objc Functions
+}
