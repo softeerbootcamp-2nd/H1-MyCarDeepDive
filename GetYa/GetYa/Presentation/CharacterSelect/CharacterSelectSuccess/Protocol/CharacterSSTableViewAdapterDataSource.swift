9e4f7c71963deaf4fd17fd35ac69e6b52e220838
shcommit <happysh_s2@naver.com>
2023-08-08 20:24:31 +0900

    feat: Character select success table view adapter data source methods 추가

---
+++ GetYa/GetYa/Presentation/CharacterSelect/CharacterSelectSuccess/Protocol/CharacterSSTableViewAdapterDataSource.swift
@@ -0,0 +1,21 @@
+//
+//  CharacterSCTableViewAdapterDataSource.swift
+//  GetYa
+//
+//  Created by 양승현 on 2023/08/08.
+//
+
+protocol CharacterSCTableViewAdapterDataSource: AnyObject {
+    var numberOfFirstSectionItems: Int { get }
+    var numberOfSectionSectionItems: Int { get }
+    func firstSectionCellItem(
+        at index: Int
+    ) -> RecommendCarProductOptionEntity
+    func secondSectionCellItem(
+        at index: Int
+    ) -> RecommendCarProductOptionEntity
+    var mainSectionHeaderItem: (
+        thumbnailKeywords: [String],
+        recommendCarProductOption: RecommendCarInfoEntity,
+        firstSectionTitle: String) { get }
+}
