9fa3c8cd12c5398d6cfaa4f5b1c4c268ffaea11c
DaeHee99 <mokday99@gmail.com>
2023-08-09 15:29:48 +0900

    feat: 다시 추천받기 팝업

--- frontend/src/Components/Modal/CloseModal/index.tsx
+++ frontend/src/Components/Modal/CloseModal/index.tsx
@@ -1,20 +1,7 @@
+import { closeModalHandler } from '@/utils';
 import closeIcon from '@/assets/icon/x-modal-icon.svg';
 
 function CloseModal() {
-  const closeModalHandler = () => {
-    const clickEvent = new MouseEvent('click', {
-      bubbles: true,
-      cancelable: true,
-      view: window,
-      clientX: 0,
-      clientY: 0,
-    });
-
-    // 특정 요소를 선택하고 마우스 클릭 이벤트 디스패치
-    const targetElement = document.elementFromPoint(0, 0);
-    targetElement?.dispatchEvent(clickEvent);
-  };
-
   return (
     <img
       src={closeIcon}
---
+++ frontend/src/Pages/TrimSelectPage/RecommendAgainModal/CloseButton/index.tsx
@@ -0,0 +1,11 @@
+import CloseModal from '@/Components/Modal/CloseModal';
+
+function CloseButton() {
+  return (
+    <div className='relative'>
+      <CloseModal />
+    </div>
+  );
+}
+
+export default CloseButton;
