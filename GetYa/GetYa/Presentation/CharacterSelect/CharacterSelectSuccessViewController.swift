88e295b25e5433497a8a76ac9084954125480cdb
shcommit <happysh_s2@naver.com>
2023-08-09 10:50:56 +0900

    feat: tableView 추가 및 adapter에 데이터 전담하도록 adapter 선언 및 vm선언

--- GetYa/GetYa/Presentation/CharacterSelect/CharacterSelectSuccessViewController.swift
+++ GetYa/GetYa/Presentation/CharacterSelect/CharacterSelectSuccessViewController.swift
@@ -9,13 +9,53 @@ import UIKit
 
 final class CharacterSelectSuccessViewController: UIViewController {
     // MARK: - Properties
-    
-    // MARK: - Lifecycles
-    init() {
-        super.init(nibName: nil, bundle: nil)
+    private let tableView: UITableView = UITableView(frame: .zero).set {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.register(
+            CharacterSelectSuccessMainHeader.self,
+            forHeaderFooterViewReuseIdentifier: CharacterSelectSuccessMainHeader.id)
+        $0.register(
+            CharacterSelectSuccessTableViewCell.self,
+            forCellReuseIdentifier: CharacterSelectSuccessTableViewCell.id)
+        $0.rowHeight = 65
+        $0.estimatedRowHeight = 50
     }
     
+    private var adapter: CharacterSelectSuccessTableViewAdapter!
+    
+    private var viewModel: (any CharacterSelectSuccessViewModelable & CharacterSSTableViewAdapterDataSource)!
+    
+    // MARK: - Lifecycles
+    init(viewModel: some CharacterSelectSuccessViewModelable & CharacterSSTableViewAdapterDataSource) {
+        super.init(nibName: nil, bundle: nil)
+        self.viewModel = viewModel
+    }
+    
+    // @available(*, unavailable)
     required init?(coder: NSCoder) {
         super.init(coder: coder)
+        viewModel = CharacterSelectSuccessViewModel()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureSubviewUI(with: tableView)
+        adapter = .init(
+            tableView: tableView,
+            dataSource: viewModel)
+    }
+}
+
+// MARK: - LayoutSupportable
+extension CharacterSelectSuccessViewController: LayoutSupportable {
+    func configureConstraints() {
+        _=tableView.set {
+            NSLayoutConstraint.activate([
+                $0.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+                $0.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+                $0.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+                $0.bottomAnchor.constraint(
+                    equalTo: view.safeAreaLayoutGuide.bottomAnchor)])
+        }
     }
 }
