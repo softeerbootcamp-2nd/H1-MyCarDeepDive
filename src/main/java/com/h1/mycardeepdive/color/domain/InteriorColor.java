b82a6ac9a6f4bdc5ae368a6df40674604f66a1f4
ajsthfldu <ajsthfldu@outlook.com>
2023-08-09 13:39:33 +0900

    feat: InteriorColor 엔티티 구현

---
+++ src/main/java/com/h1/mycardeepdive/color/domain/InteriorColor.java
@@ -0,0 +1,27 @@
+package com.h1.mycardeepdive.color.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class InteriorColor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "interior_color_id")
+    private Long id;
+
+    private String name;
+
+    private long price;
+
+    private String img_url;
+
+    private String comment;
+
+    private String interior_img_url;
+
+}
