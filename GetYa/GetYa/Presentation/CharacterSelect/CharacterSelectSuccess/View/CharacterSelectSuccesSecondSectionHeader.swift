38a6d54fd15a3ba6f6171840d2f860123c889705
shcommit <happysh_s2@naver.com>
2023-08-09 16:44:01 +0900

    feat: Character select success second section header 추가

--- GetYa/GetYa/Presentation/CharacterSelect/CharacterSelectSuccess/View/CharacterSelectSuccesSecondSectionHeader.swift
+++ GetYa/GetYa/Presentation/CharacterSelect/CharacterSelectSuccess/View/CharacterSelectSuccesSecondSectionHeader.swift
@@ -5,4 +5,40 @@
 //  Created by 양승현 on 2023/08/09.
 //
 
-import Foundation
+import UIKit
+
+final class CharacterSelectSuccesSecondSectionHeader: UITableViewHeaderFooterView {
+    static let id = String(describing: CharacterSelectSuccesSecondSectionHeader.self)
+    // MARK: - UI properties
+    private let sectionTitleAndLineDividerView = CharacterSelectSuccessSectionDividerView()
+    
+    // MARK: - Lifecycles
+    override init(reuseIdentifier: String?) {
+        super.init(reuseIdentifier: reuseIdentifier)
+        configureSubviewUI(with: sectionTitleAndLineDividerView)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    // MARK: - Functions
+    func configure(with sectionTitle: String) {
+        sectionTitleAndLineDividerView.configureSectionTitle(with: sectionTitle)
+    }
+}
+
+// MARK: - LayoutSupportable
+extension CharacterSelectSuccesSecondSectionHeader: LayoutSupportable {
+    func configureConstraints() {
+        NSLayoutConstraint.activate([
+            sectionTitleAndLineDividerView.leadingAnchor.constraint(
+                equalTo: contentView.leadingAnchor),
+            sectionTitleAndLineDividerView.topAnchor.constraint(
+                equalTo: contentView.topAnchor),
+            sectionTitleAndLineDividerView.trailingAnchor.constraint(
+                equalTo: contentView.trailingAnchor),
+            sectionTitleAndLineDividerView.bottomAnchor.constraint(
+                equalTo: contentView.bottomAnchor)])
+    }
+}
