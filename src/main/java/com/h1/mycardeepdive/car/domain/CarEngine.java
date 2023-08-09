5292d52e238bc867c4d78c4f0765eb74863096fc
Kwoun Ki Ho <fingercut3822@gmail.com>
2023-08-09 13:12:56 +0900

    feat: 다대다 CarEngine 엔티티 구현

---
+++ src/main/java/com/h1/mycardeepdive/car/domain/CarEngine.java
@@ -0,0 +1,24 @@
+package com.h1.mycardeepdive.car.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class CarEngine {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "car_engine_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "car_id")
+    private Car car;
+
+    @ManyToOne
+    @JoinColumn(name = "engine_id")
+    private Engine engine;
+}
