f8e11519e1cc916ceb2da5480e06a0c51d091867
ajsthfldu <ajsthfldu@outlook.com>
2023-08-09 13:40:29 +0900

    feat: 다대다 ColorCombination 엔티티 구현

---
+++ src/main/java/com/h1/mycardeepdive/color/domain/ColorCombination.java
@@ -0,0 +1,20 @@
+package com.h1.mycardeepdive.color.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class ColorCombination {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "color_combination_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "exterior_color_id")
+    private ExteriorColor exteriorColor;
+}
