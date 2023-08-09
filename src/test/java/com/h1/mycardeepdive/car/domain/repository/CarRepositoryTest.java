88b5b02e5513b5b2dbd797280d484716c7d93b7e
Kwoun Ki Ho <fingercut3822@gmail.com>
2023-08-09 18:25:08 +0900

    test: CarRepository 예시 수정

--- src/test/java/com/h1/mycardeepdive/car/domain/repository/CarRepositoryTest.java
+++ src/test/java/com/h1/mycardeepdive/car/domain/repository/CarRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.h1.mycardeepdive.car.domain.repository;
 
+import static java.lang.Math.abs;
 import static org.junit.jupiter.api.Assertions.*;
 
 import com.h1.mycardeepdive.car.domain.Car;
@@ -22,19 +23,36 @@ class CarRepositoryTest {
 
     @Test
     @DisplayName("이미 DB에 저장되어 있는 ID를 가진 차종을 저장하면, 해당 ID의 차종은 후에 작성된 그룹 정보로 업데이트 된다.")
-    public void testSameId() throws Exception {
+    public void testSameIdUpdate() throws Exception {
         // given
         Car car1 =
-                Car.builder().id(0L).name("그랜저IG").price(1000000000).comment("매우 좋은 현대차").build();
+                Car.builder().name("그랜저IG1").price(1000000000).comment("매우 좋은 현대차").build();
         carRepository.save(car1);
 
         // when
         Car car2 =
-                Car.builder().id(1L).name("그랜저IG").price(1000000000).comment("매우 좋은 현대차").build();
+                Car.builder().id(car1.getId()).name("그랜저IG2").price(1000000000).comment("매우 좋은 현대차").build();
         carRepository.save(car2);
 
         // then
-        List<Car> carList = carRepository.findAll();
-        assertEquals(carList.get(0).getId() - carList.get(1).getId(), 1);
+        assertEquals(car2.getName(), "그랜저IG2");
+    }
+
+
+    @Test
+    @DisplayName("id가 없는 차종 엔티티를 저장하면 순차적으로 ID를 부여하여 저장한다.")
+    public void saveNoIdMember() throws Exception {
+        // given
+        Car car1 =
+                Car.builder().name("그랜저IG1").price(1000000000).comment("매우 좋은 현대차").build();
+        carRepository.save(car1);
+
+        // when
+        Car car2 =
+                Car.builder().name("그랜저IG2").price(1000000000).comment("매우 좋은 현대차").build();
+        carRepository.save(car2);
+
+        // then
+        assertEquals(car2.getId() - car1.getId(), 1);
     }
 }
