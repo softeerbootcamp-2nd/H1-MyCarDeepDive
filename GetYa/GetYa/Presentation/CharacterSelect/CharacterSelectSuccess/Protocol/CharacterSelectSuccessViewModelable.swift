787eba2687783fb747337e9e8b90e454cae3c358
shcommit <happysh_s2@naver.com>
2023-08-09 10:50:02 +0900

    feat: CharacterSelectSuccessViewModel과 뷰에서 소통하기 위해 필요한 프로토콜 및 뷰의 Input, Output 정의

--- GetYa/GetYa/Presentation/CharacterSelect/CharacterSelectSuccess/Protocol/CharacterSelectSuccessViewModelable.swift
+++ GetYa/GetYa/Presentation/CharacterSelect/CharacterSelectSuccess/Protocol/CharacterSelectSuccessViewModelable.swift
@@ -5,4 +5,23 @@
 //  Created by 양승현 on 2023/08/09.
 //
 
-import Foundation
+import Combine
+
+struct CharacterSelectSuccessViewControllerInput {
+    let viewDidLoad: AnyPublisher<Void, Never>
+    // 이 경우 화면에서 추천된 데이터들을 기반으로 2-1, 3-1화면으로 이동해야 합니다..
+    let customButtonEvent: AnyPublisher<Void, Never>
+    let quickQuoteEvent: AnyPublisher<Void, Never>
+}
+
+enum CharacterSelectSuccessViewControllerState {
+    case none
+    case updateRecommendThumbnailKeywords
+    case gotoCustomPage
+    case gotoCompletionPage
+}
+
+protocol CharacterSelectSuccessViewModelable: ViewModelable 
+where Input == CharacterSelectSuccessViewControllerInput,
+      State == CharacterSelectSuccessViewControllerState,
+      Output == AnyPublisher<State, Never> { }
