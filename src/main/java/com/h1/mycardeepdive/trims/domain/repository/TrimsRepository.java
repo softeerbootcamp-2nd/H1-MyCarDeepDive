fca077c53c136a46ab98c0a4431910d8d46b01ed
Kwoun Ki Ho <fingercut3822@gmail.com>
2023-08-09 17:06:35 +0900

    feat: TrimsRepository 생성

---
+++ src/main/java/com/h1/mycardeepdive/trims/domain/repository/TrimsRepository.java
@@ -0,0 +1,7 @@
+package com.h1.mycardeepdive.trims.domain.repository;
+
+import com.h1.mycardeepdive.trims.domain.Trims;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TrimsRepository extends JpaRepository<Trims, Long> {
+}
