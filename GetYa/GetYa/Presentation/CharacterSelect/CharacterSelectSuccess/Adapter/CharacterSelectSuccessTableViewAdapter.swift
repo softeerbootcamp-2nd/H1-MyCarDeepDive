593865f12b98377437175fcc3ff34f78625c5c53
shcommit <happysh_s2@naver.com>
2023-08-11 00:17:28 +0900

    feat: header, footer 아래로 스크롤 안되게 제한. + header view 추가

--- GetYa/GetYa/Presentation/CharacterSelect/CharacterSelectSuccess/Adapter/CharacterSelectSuccessTableViewAdapter.swift
+++ GetYa/GetYa/Presentation/CharacterSelect/CharacterSelectSuccess/Adapter/CharacterSelectSuccessTableViewAdapter.swift
@@ -72,6 +72,7 @@ extension CharacterSelectSuccessTableViewAdapter: UITableViewDelegate {
         isWorkedMainHeaderInitialAnimation.toggle()
         header.showAnimation()
     }
+    
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         if section == 0, let header = tableView.dequeueReusableHeaderFooterView(
             withIdentifier: CharacterSelectSuccessMainHeader.id
@@ -80,7 +81,7 @@ extension CharacterSelectSuccessTableViewAdapter: UITableViewDelegate {
         } else if section == 1, let header = tableView.dequeueReusableHeaderFooterView(
             withIdentifier: CharacterSelectSuccesSecondSectionHeader.id
         ) as? CharacterSelectSuccesSecondSectionHeader {
-            return header.set { $0.configure(with: dataSource.seciondSectionHeaderItem) }
+            return header.set { $0.configure(with: dataSource.secondSectionHeaderItem) }
         }
         return .init(frame: .zero)
     }
@@ -95,7 +96,33 @@ extension CharacterSelectSuccessTableViewAdapter: UITableViewDelegate {
         return 0
     }
     
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        if section == 1, let footer = tableView.dequeueReusableHeaderFooterView(
+            withIdentifier: CharacterSelectSuccessFooter.id
+        ) as? CharacterSelectSuccessFooter {
+            return footer.set {
+                $0.configure(with: dataSource.secondSectionFooterItem)
+            }
+        }
+        return nil
+    }
+    
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return CharacterSelectSuccessTableViewCell.Constant.intrinsicContentHeight
+    }
+}
+
+// MARK: - UIScrollViewDelegate
+extension CharacterSelectSuccessTableViewAdapter {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let offSetY = scrollView.contentOffset.y
+        let svContentHeight = scrollView.contentSize.height
+        let scrollableMaximumY = svContentHeight - scrollView.bounds.height
+        guard scrollableMaximumY > 0 else { return }
+        if offSetY < 0 {
+            scrollView.contentOffset.y = 0
+        } else if offSetY > scrollableMaximumY {
+            scrollView.contentOffset.y = scrollableMaximumY
+        }
     }
 }
