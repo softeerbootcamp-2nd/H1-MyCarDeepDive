6f616b70be9a78c45fa2c068530419aa462258b7
NamSeok-Bae <qoskatjr2@gmail.com>
2023-08-10 14:56:03 +0900

    add: TrimSubOptionButton 생성

---
+++ GetYa/GetYa/Presentation/TrimSelect/View/TrimSubOptionButton.swift
@@ -0,0 +1,20 @@
+//
+//  TrimSubOptionButton.swift
+//  GetYa
+//
+//  Created by 배남석 on 2023/08/10.
+//
+
+import UIKit
+
+class TrimSubOptionButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
