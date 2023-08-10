277e3d596f454b52a0e8097be7c8c83df97a600c
Kwoun Ki Ho <fingercut3822@gmail.com>
2023-08-10 16:46:12 +0900

    feat: Tags 엔티티 구현 및 테스트

---
+++ src/main/java/com/h1/mycardeepdive/tags/domain/Tags.java
@@ -0,0 +1,42 @@
+package com.h1.mycardeepdive.tags.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@NoArgsConstructor
+@Getter
+public class Tags {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "tag_id")
+    private Long id;
+
+    private String name;
+
+    private String img_url;
+
+    @Builder
+    public Tags(Long id, String name, String img_url) {
+        this.id = id;
+        this.name = name;
+        this.img_url = img_url;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Tags tag = (Tags) o;
+        return Objects.equals(id, tag.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ src/test/java/com/h1/mycardeepdive/tags/domain/TagsTest.java
@@ -0,0 +1,50 @@
+package com.h1.mycardeepdive.tags.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class TagsTest {
+    @DisplayName("태그를 빌더 패턴을 활용하여 생성할 수 있다")
+    @Test
+    public void createTags() throws Exception {
+        //given
+        Long id = 1L;
+        String name = "tag_name";
+        String img_url = "img.jpg";
+
+        //when
+        Tags tags = Tags.builder()
+                .id(id)
+                .name(name)
+                .img_url(img_url)
+                .build();
+
+        //then
+        assertThat(tags.getId()).isEqualTo(id);
+        assertThat(tags.getName()).isEqualTo(name);
+        assertThat(tags.getImg_url()).isEqualTo(img_url);
+    }
+
+    @DisplayName("태그 도메인의 hashCode/equals는 id 값이 같다면 같은 것으로 처리된다.")
+    @Test
+    void tagEqualsById() {
+        // given
+        Long id = 1L;
+
+        // when
+        Tags tag1 = Tags.builder().id(id).name("artist1").build();
+        Tags tag2 = Tags.builder().id(id).name("artist2").build();
+        Set<Tags> tagSet = new HashSet<>(Arrays.asList(tag1, tag2));
+
+        // then
+        assertThat(tag1).isEqualTo(tag2);
+        assertThat(tag1.hashCode()).isEqualTo(tag2.hashCode());
+        assertThat(tagSet.size()).isEqualTo(1);
+    }
+}
