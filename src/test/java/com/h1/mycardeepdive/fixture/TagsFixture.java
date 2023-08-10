4ef09a8080abbbe3481acdb2d2aa99a012b85c08
Kwoun Ki Ho <fingercut3822@gmail.com>
2023-08-11 00:19:52 +0900

    feat: OptionTag 엔티티 생성 및 테스트

--- src/test/java/com/h1/mycardeepdive/fixture/TagsFixture.java
+++ src/test/java/com/h1/mycardeepdive/fixture/TagsFixture.java
@@ -3,13 +3,9 @@ package com.h1.mycardeepdive.fixture;
 import com.h1.mycardeepdive.tags.domain.Tags;
 
 public class TagsFixture {
-    private TagsFixture() {
-    }
+    private TagsFixture() {}
 
     public static Tags createSafetyTag() {
-        return Tags.builder()
-                .name("주행안전")
-                .img_url("safety_car.jpg")
-                .build();
+        return Tags.builder().name("주행안전").img_url("safety_car.jpg").build();
     }
 }
