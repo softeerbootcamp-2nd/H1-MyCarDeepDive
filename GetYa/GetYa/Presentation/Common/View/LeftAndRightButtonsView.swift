c42f45db989d2cd0bdd9fe350122337ae53cfd62
shcommit <happysh_s2@naver.com>
2023-08-11 09:41:22 +0900

    feat: 왼쪽, 오른쪽 버튼 쌍으로 있는 컴포넌트 생성!!

--- GetYa/GetYa/Presentation/Common/View/LeftAndRightButtonsView.swift
+++ GetYa/GetYa/Presentation/Common/View/LeftAndRightButtonsView.swift
@@ -1,8 +1,124 @@
 //
-//  LeftRightButtonView.swift
+//  LeftAndRightButtonsView.swift
 //  GetYa
 //
 //  Created by 양승현 on 2023/08/11.
 //
 
-import Foundation
+import UIKit
+import Combine
+
+protocol LeftAndRightButtonsViewDelegate: AnyObject {
+    func didTapLeftButton()
+    func didTapRightButton()
+}
+
+class LeftAndRightButtonsView: UIStackView {
+    struct ButtonAppearance {
+        let text: String
+        let fontType: GetYaFont
+        let backgroundColor: UIColor
+        let borderColor: UIColor
+        let textColor: UIColor
+    }
+    
+    // MARK: - UI Properties
+    private let leftButton = CommonButton(frame: .zero)
+    private let rightButton = CommonButton(frame: .zero)
+    
+    // MARK: - Properties
+    weak var delegate: LeftAndRightButtonsViewDelegate?
+    private var subscriptions = Set<AnyCancellable>()
+    
+    // MARK: - Lifecycles
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureUI()
+        bind()
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+        configureUI()
+        bind()
+    }
+    
+    deinit {
+        _=subscriptions.map { $0.cancel() }
+    }
+    
+    // MARK: - Private Functions
+    private func configureUI() {
+        let leftBtnAppearance = ButtonAppearance(
+            text: "이전",
+            fontType: .mediumBody3,
+            backgroundColor: .white,
+            borderColor: .GetYaPalette.gray600,
+            textColor: .black)
+        let rightBtnAppearacne = ButtonAppearance(
+            text: "다음",
+            fontType: .mediumBody3,
+            backgroundColor: .GetYaPalette.acriveBlue,
+            borderColor: .clear,
+            textColor: .white)
+        
+        configureLeftButton(with: leftBtnAppearance)
+        configureRightButton(with: rightBtnAppearacne)
+        translatesAutoresizingMaskIntoConstraints = false
+        axis = .horizontal
+        distribution = .fillEqually
+        spacing = 6
+        _ = [leftButton, rightButton].map { addArrangedSubview($0) }
+    }
+    
+    private func bind() {
+        leftButton.tap.sink { [weak self] in
+            self?.touchAnimation(target: self?.leftButton) {
+                self?.delegate?.didTapLeftButton()
+            }
+        }.store(in: &subscriptions)
+        
+        rightButton.tap.sink { [weak self] in
+            self?.touchAnimation(target: self?.rightButton) {
+                self?.delegate?.didTapRightButton()
+            }
+        }.store(in: &subscriptions)
+    }
+    
+    // MARK: - Functions
+    func configureLeftButton(with: ButtonAppearance) {
+        leftButton.configureDetail(
+            text: with.text,
+            fontType: with.fontType,
+            backgroundColor: with.backgroundColor,
+            borderColor: with.borderColor,
+            textColor: with.textColor)
+    }
+    
+    func configureRightButton(with: ButtonAppearance) {
+        rightButton.configureDetail(
+            text: with.text,
+            fontType: with.fontType,
+            backgroundColor: with.backgroundColor,
+            borderColor: with.borderColor,
+            textColor: with.textColor)
+    }
+    
+    /// 왼쪽버튼과 오른쪽 버튼사이 간격 설정하기!!
+    func setSpacing(_ spacing: CGFloat) {
+        self.spacing = spacing
+    }
+    
+    func touchAnimation(target: UIButton?, completionHandler: @escaping () -> Void) {
+        UIView.animate(
+            withDuration: 0.07,
+            delay: 0,
+            options: .curveEaseInOut,
+            animations: {
+                target?.alpha = 0.777
+            }, completion: { _ in
+                completionHandler()
+                UIView.animate(withDuration: 0.12, animations: { target?.alpha = 1 })
+            })
+    }
+}
