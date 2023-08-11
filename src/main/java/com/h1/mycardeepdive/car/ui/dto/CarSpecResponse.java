1e272cfe261e6f7b9c8963d455e96667b0fbf3a7
ajsthfldu <ajsthfldu@outlook.com>
2023-08-11 11:20:27 +0900

    feat: CarSpecResponse 생성

---
+++ src/main/java/com/h1/mycardeepdive/car/ui/dto/CarSpecResponse.java
@@ -0,0 +1,20 @@
+package com.h1.mycardeepdive.car.ui.dto;
+
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CarSpecResponse {
+    private String trims_name;
+    private String price;
+    private String summary;
+    private Long car_spec_id;
+    private Long trims_id;
+    private List<String> basic_option_names;
+    private List<Long> basic_option_ids;
+}
