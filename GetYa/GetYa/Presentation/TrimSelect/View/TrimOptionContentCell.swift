6ec8cf686757cdf3905cf63ea2f44c15ae370543
NamSeok-Bae <qoskatjr2@gmail.com>
2023-08-11 11:09:25 +0900

    add: TrimOptionContentCell 생성

---
+++ GetYa/GetYa/Presentation/TrimSelect/View/TrimOptionContentCell.swift
@@ -0,0 +1,29 @@
+//
+//  TrimOptionContentCell.swift
+//  GetYa
+//
+//  Created by 배남석 on 2023/08/11.
+//
+
+import UIKit
+
+class TrimOptionContentCell: UICollectionViewCell {
+    // MARK: - UI properties
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycles
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    // MARK: - Private Functions
+    
+    // MARK: - Functions
+    
+    // MARK: - Objc Functions
+}
