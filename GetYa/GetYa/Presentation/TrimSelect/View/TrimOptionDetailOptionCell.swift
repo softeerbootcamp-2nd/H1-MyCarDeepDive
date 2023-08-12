259d136d8a79c4b202bfcbf889873d00a139ec7c
NamSeok-Bae <qoskatjr2@gmail.com>
2023-08-12 19:14:27 +0900

    add: TrimOptionDetailOptionCollectionView, TrimOptionDetailOptionCell 생성

---
+++ GetYa/GetYa/Presentation/TrimSelect/View/TrimOptionDetailOptionCell.swift
@@ -0,0 +1,12 @@
+//
+//  TrimOptionDetailOptionCell.swift
+//  GetYa
+//
+//  Created by 배남석 on 2023/08/12.
+//
+
+import UIKit
+
+class TrimOptionDetailOptionCell: UICollectionViewCell {
+    
+}
---
+++ GetYa/GetYa/Presentation/TrimSelect/View/TrimOptionDetailOptionCollectionView.swift
@@ -0,0 +1,20 @@
+//
+//  TrimOptionDetailOptionCollectionView.swift
+//  GetYa
+//
+//  Created by 배남석 on 2023/08/12.
+//
+
+import UIKit
+
+class TrimOptionDetailOptionCollectionView: UICollectionView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
