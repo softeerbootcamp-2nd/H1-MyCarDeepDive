42f04afc3cbefd793f4e321ceb538b0b30f42079
NamSeok-Bae <qoskatjr2@gmail.com>
2023-08-12 19:04:58 +0900

    add: TrimOptionDetailColorCell 생성 및 구현

---
+++ GetYa/GetYa/Presentation/TrimSelect/View/TrimOptionDetailColorCell.swift
@@ -0,0 +1,42 @@
+//
+//  TrimOptionDetailColorCell.swift
+//  GetYa
+//
+//  Created by 배남석 on 2023/08/12.
+//
+
+import UIKit
+
+class TrimOptionDetailColorCell: UICollectionViewCell {
+    // MARK: - Properties
+    static let identifier = "TrimOptionDetailColorCell"
+    
+    // MARK: - Lifecycles
+    convenience init(color: UIColor) {
+        self.init(frame: .zero)
+        
+        configureBackgroundColor(color: color)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configureUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        configureUI()
+    }
+    
+    // MARK: - Private Functions
+    private func configureUI() {
+        layer.cornerRadius = CGFloat(2).scaledHeight
+    }
+    
+    // MARK: - Functions
+    func configureBackgroundColor(color: UIColor) {
+        layer.backgroundColor = color.cgColor
+    }
+}
