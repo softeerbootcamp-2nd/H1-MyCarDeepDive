673ce6b12a2df71421faa2df04177f315771918e
NamSeok-Bae <qoskatjr2@gmail.com>
2023-08-12 12:25:09 +0900

    feat: TrimLearnMoreView 구현

---
+++ GetYa/GetYa/Presentation/TrimSelect/View/TrimLearnMoreView.swift
@@ -0,0 +1,113 @@
+//
+//  TrimLearnMoreView.swift
+//  GetYa
+//
+//  Created by 배남석 on 2023/08/12.
+//
+
+import UIKit
+
+class TrimLearnMoreView: LearnMoreView {
+    enum Constants {
+        enum Label {
+            static let topMargin = CGFloat(12).scaledHeight
+            static let leadingMargin = CGFloat(12).scaledWidth
+        }
+        enum ContentView {
+            static let topMargin = CGFloat(17).scaledHeight
+            static let leadingMargin = CGFloat(20).scaledWidth
+            static let trailingMargin = CGFloat(-20).scaledWidth
+            static let height = CGFloat(285).scaledHeight
+        }
+    }
+    
+    // MARK: - UI properties
+    private let label = CommonLabel(fontType: .mediumBody4, color: .GetYaPalette.primary)
+    private var contentView: UIView = UIView().set {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.backgroundColor = .red
+    }
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycles
+    init(textColor: UIColor, text: String) {
+        super.init(frame: .zero)
+        
+        setupViews()
+        configureUI()
+        configureTextColor(color: textColor)
+        configureText(text: text)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupViews()
+        configureUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        setupViews()
+        configureUI()
+    }
+    
+    // MARK: - Private Functions
+    private func setupViews() {
+        addSubviews([
+            label,
+            contentView
+        ])
+    }
+    
+    private func configureUI() {
+        clipsToBounds = true
+        
+        configureLabel()
+        configureContentView()
+    }
+    
+    private func configureContentView() {
+        NSLayoutConstraint.activate([
+            contentView.topAnchor.constraint(
+                equalTo: label.bottomAnchor,
+                constant: Constants.ContentView.topMargin),
+            contentView.leadingAnchor.constraint(
+                equalTo: leadingAnchor,
+                constant: Constants.ContentView.leadingMargin),
+            contentView.trailingAnchor.constraint(
+                equalTo: trailingAnchor,
+                constant: Constants.ContentView.trailingMargin),
+            contentView.heightAnchor.constraint(
+                equalToConstant: Constants.ContentView.height)
+        ])
+    }
+    
+    private func configureLabel() {
+        NSLayoutConstraint.activate([
+            label.topAnchor.constraint(
+                equalTo: topAnchor,
+                constant: Constants.Label.topMargin),
+            label.leadingAnchor.constraint(
+                equalTo: leadingAnchor,
+                constant: Constants.Label.leadingMargin)
+        ])
+    }
+    
+    // MARK: - Functions
+    func configureText(text: String) {
+        label.text = text
+    }
+    
+    func configureTextColor(color: UIColor) {
+        label.textColor = color
+    }
+    
+    func setContentView(view: UIView) {
+        contentView = view
+    }
+    
+    // MARK: - Objc Functions
+}
