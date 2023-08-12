70529529153e80a5faf24bcbee2baf300207f8ef
NamSeok-Bae <qoskatjr2@gmail.com>
2023-08-13 03:55:44 +0900

    add: BottomSheetSmallContentView, BottomSheetLargeContentView

---
+++ GetYa/GetYa/Presentation/TrimSelect/View/BottomSheetLargeContentView.swift
@@ -0,0 +1,23 @@
+//
+//  BottomSheetLargeContentView.swift
+//  GetYa
+//
+//  Created by 배남석 on 2023/08/13.
+//
+
+import UIKit
+
+class BottomSheetLargeContentView: UIView {
+    // MARK: - UI properties
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycles
+    
+    // MARK: - Private Functions
+    
+    // MARK: - Functions
+    
+    // MARK: - Objc Functions 
+
+}
