07bcf3c1f976abda85096b4fa13c64b605881fff
DaeHee99 <mokday99@gmail.com>
2023-08-12 23:42:04 +0900

    design: 컨트롤 버튼 투명도 부여

--- frontend/src/Pages/TrimSelectionPage/Car/ControlButtons/index.tsx
+++ frontend/src/Pages/TrimSelectionPage/Car/ControlButtons/index.tsx
@@ -10,20 +10,20 @@ interface ControlButtonsProps {
 function ControlButtons({ rotation, setRotation }: ControlButtonsProps) {
   return (
     <div className='absolute top-24 left-[10%] flex flex-col gap-2 z-40'>
-      <button className='w-[52px] h-[52px] shadow-md bg-grey-1000 rounded px-2.5 flex justify-between items-center hover:w-24 group transition-width duration-300'>
+      <button className='w-[52px] h-[52px] shadow-md bg-grey-1000 bg-opacity-80 rounded px-2.5 flex justify-between items-center hover:w-24 group transition-width duration-300'>
         <img src={exterial} alt='exterial' />
         <span className='font-caption1-medium text-grey-0 mr-2 opacity-0 group-hover:opacity-100 transition-opacity duration-1000'>
           외장
         </span>
       </button>
-      <button className='w-[52px] h-[52px] shadow-md bg-grey-1000 rounded px-2.5 flex justify-between items-center hover:w-24 group transition-width duration-300'>
+      <button className='w-[52px] h-[52px] shadow-md bg-grey-1000 bg-opacity-80 rounded px-2.5 flex justify-between items-center hover:w-24 group transition-width duration-300'>
         <img src={interial} alt='interial' />
         <span className='font-caption1-medium text-grey-0 mr-2 opacity-0 group-hover:opacity-100 transition-opacity duration-1000'>
           내장
         </span>
       </button>
       <button
-        className='w-[52px] h-[52px] shadow-md bg-grey-1000 rounded px-2.5 flex justify-between items-center hover:w-24 group transition-width duration-300'
+        className='w-[52px] h-[52px] shadow-md bg-grey-1000 bg-opacity-80 rounded px-2.5 flex justify-between items-center hover:w-24 group transition-width duration-300'
         onClick={() => setRotation(!rotation)}
       >
         <img src={rotation360} alt='ratation' />
