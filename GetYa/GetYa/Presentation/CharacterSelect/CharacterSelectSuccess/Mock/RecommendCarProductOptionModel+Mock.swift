b74fb77f82b932b1d398e101dc77978b34230f9c
shcommit <happysh_s2@naver.com>
2023-08-13 21:11:46 +0900

    chore: 서버에서 올 수 있는 데이터 행 개수 1~2~3개로 재설정

--- GetYa/GetYa/Presentation/CharacterSelect/CharacterSelectSuccess/Mock/RecommendCarProductOptionModel+Mock.swift
+++ GetYa/GetYa/Presentation/CharacterSelect/CharacterSelectSuccess/Mock/RecommendCarProductOptionModel+Mock.swift
@@ -9,11 +9,11 @@ import Foundation
 
 extension RecommendCarProductOptionModel {
     fileprivate static let colorProducts: [Self] = [
-        .init(optionImage: "recommendOptionBlackColor",
+        RecommendCarProductOptionModel(optionImage: "recommendOptionBlackColor",
               optionName: "외장 - 크리미 화이트 펄",
               optionPrice: 0,
               optionReview: "75%의 20~30대 구매자들이 선택했어요"),
-        .init(optionImage: "recommendOptioncolorchip",
+        RecommendCarProductOptionModel(optionImage: "recommendOptioncolorchip",
               optionName: "내장 - 인조가죽 (블랙)",
               optionPrice: 2000000,
               optionReview: "75%의 20~30대 구매자들이 선택했어요")]
@@ -24,16 +24,16 @@ extension RecommendCarProductOptionModel {
               optionPrice: 1090000,
               optionReview: "해당 옵션이 후석 승객 알림이 있어서 좋아요. 뒷자리 아이들을 확인할 수 있거든요."),
         .init(optionImage: "recommendCarOption",
-              optionName: "현대 스마트센스 I",
-              optionPrice: 790000,
-              optionReview: "전방 충돌 방지 보조 기능이 있어 안전을 위해서라면 무조건 추가해야 하는 옵션이에요."),
+              optionName: "현대 스마트센스 III",
+              optionPrice: 1190000,
+              optionReview: "전방 충돌 방지 보조 기능이 있어 안전을 위해서라면 무조건 추가해야 하는 옵션이에요. 안전을 위해서라도 강력 추천 합니다. 후회하지 않는 선택이 되실거에요."),
         .init(optionImage: "recommendOptioncolorchip",
               optionName: "컴포트 III",
               optionPrice: 1390000,
               optionReview: "해당 옵션이 후석 승객 알림이 있어서 좋아요. 뒷자리 아이들을 확인할 수 있거든요."),
         .init(optionImage: "recommendCarOption",
-              optionName: "현대 스마트센스 II",
-              optionPrice: 1190000,
+              optionName: "현대 스마트센스 I",
+              optionPrice: 790000,
               optionReview: "전방 충돌 방지 보조 기능이 있어 안전을 위해서라면 무조건 추가해야 하는 옵션이에요.")]
     
     static let mocks: [[Self]] = [
