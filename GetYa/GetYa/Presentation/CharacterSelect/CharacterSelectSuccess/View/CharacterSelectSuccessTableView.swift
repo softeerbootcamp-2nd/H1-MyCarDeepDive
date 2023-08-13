903b7a2f92c1fe6809279de857f652d697e7c1e7
shcommit <happysh_s2@naver.com>
2023-08-14 02:38:27 +0900

    feat: CharacterSelectSuccessTableView 구현

--- GetYa/GetYa/Presentation/CharacterSelect/CharacterSelectSuccess/View/CharacterSelectSuccessTableView.swift
+++ GetYa/GetYa/Presentation/CharacterSelect/CharacterSelectSuccess/View/CharacterSelectSuccessTableView.swift
@@ -5,4 +5,32 @@
 //  Created by 양승현 on 2023/08/14.
 //
 
-import Foundation
+import UIKit
+
+final class CharacterSelectSuccessTableView: CommonRecommendResultTableView {
+    // MARK: - Lifecycles
+    override init(frame: CGRect, style: UITableView.Style) {
+        super.init(frame: frame, style: style)
+        configureUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        configureUI()
+    }
+    
+    // MARK: - Private functions
+    private func configureUI() {
+        register(
+            CharacterSelectSuccessMainHeader.self,
+            forHeaderFooterViewReuseIdentifier: CharacterSelectSuccessMainHeader.identifier)
+        register(
+            CharacterSelectSuccesSecondSectionHeader.self,
+            forHeaderFooterViewReuseIdentifier: CharacterSelectSuccesSecondSectionHeader.identifier)
+        register(
+            CharacterSelectSuccessFooter.self,
+            forHeaderFooterViewReuseIdentifier: CharacterSelectSuccessFooter.identifier)
+        estimatedSectionHeaderHeight = UITableView.automaticDimension
+        estimatedSectionFooterHeight = UITableView.automaticDimension
+    }
+}
