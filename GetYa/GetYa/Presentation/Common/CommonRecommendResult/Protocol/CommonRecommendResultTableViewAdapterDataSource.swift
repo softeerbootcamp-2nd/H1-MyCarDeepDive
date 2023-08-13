7a19fbbe6c450ec5e315edb1cfa589c3e62cc2b7
shcommit <happysh_s2@naver.com>
2023-08-14 00:34:34 +0900

    add: CommonRecommendResultTableViewAdapter 생성및 TableViewAdapterDataSource로 이름 변경

--- GetYa/GetYa/Presentation/Common/CommonRecommendResult/Protocol/CommonRecommendResultTableViewAdapterDataSource.swift
+++ GetYa/GetYa/Presentation/Common/CommonRecommendResult/Protocol/CommonRecommendResultTableViewAdapterDataSource.swift
@@ -5,7 +5,7 @@
 //  Created by 양승현 on 2023/08/14.
 //
 
-protocol CommonRecommendResultTableViewDataSource {
+protocol CommonRecommendResultTableViewAdapterDataSource {
     func cellItem(in section: Int, _ row: Int) -> RecommendCarProductOptionModel
     func numberOfRows(in section: Int) -> Int
     var numberOfSections: Int { get }
