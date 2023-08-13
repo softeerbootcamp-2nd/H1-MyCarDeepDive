c1716da3226c51cc490b53d76c7b5503394b6e51
shcommit <happysh_s2@naver.com>
2023-08-14 00:24:27 +0900

    feat: CommonRecommendResultTableView에서 필요한 dataSource protocol로 정의

--- GetYa/GetYa/Presentation/Common/CommonRecommendResult/Protocol/CommonRecommendResultTableViewDataSource.swift
+++ GetYa/GetYa/Presentation/Common/CommonRecommendResult/Protocol/CommonRecommendResultTableViewDataSource.swift
@@ -5,4 +5,8 @@
 //  Created by 양승현 on 2023/08/14.
 //
 
-import Foundation
+protocol CommonRecommendResultTableViewDataSource {
+    func cellItem(in section: Int, _ row: Int) -> RecommendCarProductOptionModel
+    func numberOfRows(in section: Int) -> Int
+    var numberOfSections: Int { get }
+}
