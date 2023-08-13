797e8b3894169b98f655d049c85043c2c6c9b114
ajsthfldu <ajsthfldu@outlook.com>
2023-08-13 18:53:41 +0900

    feat: Trims에 빌더 메소드 추가

--- src/main/java/com/h1/mycardeepdive/trims/domain/Trims.java
+++ src/main/java/com/h1/mycardeepdive/trims/domain/Trims.java
@@ -2,12 +2,16 @@ package com.h1.mycardeepdive.trims.domain;
 
 import com.h1.mycardeepdive.car.domain.Car;
 import javax.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
 @NoArgsConstructor
+@Builder
+@AllArgsConstructor
 public class Trims {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
