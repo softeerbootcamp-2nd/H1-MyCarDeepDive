e775f5bee2ccee7ca3d6e41a7c7d89768bd5f1f7
shcommit <happysh_s2@naver.com>
2023-08-14 02:45:23 +0900

    feat: 불필요한 함수 제거

--- GetYa/GetYa/Presentation/CharacterSelect/CharacterSelectSuccess/Protocol/CharacterSSTableViewAdapterDataSource.swift
+++ GetYa/GetYa/Presentation/CharacterSelect/CharacterSelectSuccess/Protocol/CharacterSSTableViewAdapterDataSource.swift
@@ -6,9 +6,6 @@
 //
 
 protocol CharacterSSTableViewAdapterDataSource: AnyObject {
-    var numberOfSections: Int { get }
-    func numberOfRows(in section: Int) -> Int
-    func cellItem(in section: Int, _ row: Int) -> RecommendCarProductOptionModel
     var mainSectionHeaderItem: MainSectionHeaderModel { get }
     var secondSectionHeaderItem: String { get }
     var secondSectionFooterItem: String { get }
