2a4497ee5d5c5e6f9775aceeaa85d25ef875ce74
shcommit <happysh_s2@naver.com>
2023-08-14 00:36:15 +0900

    feat: CommonRecommnedResultTableViewAdapter 구현

--- GetYa/GetYa/Presentation/Common/CommonRecommendResult/Adapter/CommonRecommendResultTableViewAdapter.swift
+++ GetYa/GetYa/Presentation/Common/CommonRecommendResult/Adapter/CommonRecommendResultTableViewAdapter.swift
@@ -5,4 +5,50 @@
 //  Created by 양승현 on 2023/08/14.
 //
 
-import Foundation
+import UIKit
+
+class CommonRecommendResultTableViewAdapter: NSObject {
+    // MARK: - Properties
+    private var dataSource: CommonRecommendResultTableViewAdapterDataSource
+    
+    // MARK: - Lifecycles
+    init(
+        tableView: UITableView,
+        dataSource: CommonRecommendResultTableViewAdapterDataSource
+    ) {
+        self.dataSource = dataSource
+        super.init()
+        tableView.dataSource = self
+    }
+}
+
+// MARK: - UITableViewDataSource
+extension CommonRecommendResultTableViewAdapter: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return dataSource.numberOfSections
+    }
+    
+    func tableView(
+        _ tableView: UITableView,
+        numberOfRowsInSection section: Int
+    ) -> Int {
+        return dataSource.numberOfRows(in: section)
+    }
+    
+    func tableView(
+        _ tableView: UITableView,
+        cellForRowAt indexPath: IndexPath
+    ) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(
+            withIdentifier: CommonRecommendResultTableViewCell.identifier,
+            for: indexPath
+        ) as? CommonRecommendResultTableViewCell else {
+            return .init(
+                style: .default,
+                reuseIdentifier: CommonRecommendResultTableViewCell.identifier)
+        }
+        let item = dataSource.cellItem(in: indexPath.section, indexPath.row)
+        cell.configure(with: item)
+        return cell
+    }
+}
