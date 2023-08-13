90fa218df719238d3e5bba7c1229143d486924b6
shcommit <happysh_s2@naver.com>
2023-08-14 00:23:59 +0900

    feat: CommonRecommendResultTableViewModel 추가

--- GetYa/GetYa/Presentation/Common/CommonRecommendResult/ViewModel/CommonRecommendResultTableViewModel.swift
+++ GetYa/GetYa/Presentation/Common/CommonRecommendResult/ViewModel/CommonRecommendResultTableViewModel.swift
@@ -5,4 +5,40 @@
 //  Created by 양승현 on 2023/08/14.
 //
 
-import Foundation
+// MARK: - View model
+struct RecommendCarProductOptionModel {
+    let optionImage: String?
+    let optionName: String?
+    let optionPrice: Int?
+    let optionReview: String?
+}
+
+// MARK: - ViewModel
+class CommonRecommendResultTableViewModel {
+    // MARK: - Properties
+    private(set) var dataSource: [[RecommendCarProductOptionModel]]
+    
+    // MARK: - Lifecycles
+    init(dataSource: [[RecommendCarProductOptionModel]]) {
+        self.dataSource = dataSource
+    }
+    
+    init() {
+        dataSource = RecommendCarProductOptionModel.mocks
+    }
+}
+
+// MARK: - CommonRecommendResultTableViewDataSource
+extension CommonRecommendResultTableViewModel: CommonRecommendResultTableViewDataSource {
+    func cellItem(in section: Int, _ row: Int) -> RecommendCarProductOptionModel {
+        return dataSource[section][row]
+    }
+    
+    func numberOfRows(in section: Int) -> Int {
+        return dataSource[section].count
+    }
+    
+    var numberOfSections: Int {
+        dataSource.count
+    }
+}
