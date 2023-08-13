e343d34973973c8159deb33d4aa688743782ea82
NamSeok-Bae <qoskatjr2@gmail.com>
2023-08-14 02:36:45 +0900

    add: CarSettingSelectViewController 생성

---
+++ GetYa/GetYa/Presentation/TrimSelect/CarSettingSelectViewController.swift
@@ -0,0 +1,22 @@
+//
+//  CarSettingSelectViewController.swift
+//  GetYa
+//
+//  Created by 배남석 on 2023/08/14.
+//
+
+import UIKit
+
+class CarSettingSelectViewController: UIView {
+    // MARK: - UI properties
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycles
+    
+    // MARK: - Private Functions
+    
+    // MARK: - Functions
+    
+    // MARK: - Objc Functions
+}
