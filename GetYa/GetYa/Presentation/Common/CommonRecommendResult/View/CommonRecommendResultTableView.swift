53fdbf2819ef0f1c1c4fecc5843d59cc30b60a48
shcommit <happysh_s2@naver.com>
2023-08-13 23:58:29 +0900

    feat: CommonRecommendResultTableView 생성

--- GetYa/GetYa/Presentation/Common/CommonRecommendResult/View/CommonRecommendResultTableView.swift
+++ GetYa/GetYa/Presentation/Common/CommonRecommendResult/View/CommonRecommendResultTableView.swift
@@ -8,5 +8,40 @@
 import UIKit
 
 class CommonRecommendResultTableView: UITableView {
+    // MARK: - Properties
+    private var calculateMaximumCellHeight: CGFloat {
+        let maximumReviewdTextViewHeightAndTopMargin = {
+            typealias Const = CommonRecommendResultTableViewCell.Constants.ReviewdTextView
+            return Const.maximumHeight + Const.topMargin
+        }()
+        let commonOptionViewHeightAndTopMargin = {
+            typealias Const = CommonRecommendResultTableViewCell.Constants.RecommendCarOptionView
+            return Const.height + Const.topMargin
+        }()
+        return maximumReviewdTextViewHeightAndTopMargin + commonOptionViewHeightAndTopMargin
+    }
     
+    // MARK: - Lifecycles
+    override init(frame: CGRect, style: UITableView.Style) {
+        super.init(frame: frame, style: style)
+        configureUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    // MARK: - Private Functions
+    private func configureUI() {
+        if frame != .zero {
+            translatesAutoresizingMaskIntoConstraints = false
+        }
+        backgroundColor = .white
+        register(
+            CommonRecommendResultTableViewCell.self,
+            forCellReuseIdentifier: CommonRecommendResultTableViewCell.identifier)
+        separatorStyle = .none
+        estimatedRowHeight = calculateMaximumCellHeight
+        
+    }
 }
