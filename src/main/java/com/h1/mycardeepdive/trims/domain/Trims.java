41982d59da325ffdc2fdb41a7fdf2df2e1708b5d
ajsthfldu <ajsthfldu@outlook.com>
2023-08-14 15:26:49 +0900

    feat: TrimsColorCombination으로 OneToMany매핑 추가

--- src/main/java/com/h1/mycardeepdive/trims/domain/Trims.java
+++ src/main/java/com/h1/mycardeepdive/trims/domain/Trims.java
@@ -1,6 +1,8 @@
 package com.h1.mycardeepdive.trims.domain;
 
 import com.h1.mycardeepdive.car.domain.Car;
+import com.h1.mycardeepdive.color.domain.TrimsColorCombination;
+import java.util.List;
 import javax.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -39,4 +41,7 @@ public class Trims {
     @ManyToOne
     @JoinColumn(name = "car_id")
     private Car car;
+
+    @OneToMany(mappedBy = "trims", fetch = FetchType.LAZY)
+    private List<TrimsColorCombination> trimsColorCombinations;
 }
