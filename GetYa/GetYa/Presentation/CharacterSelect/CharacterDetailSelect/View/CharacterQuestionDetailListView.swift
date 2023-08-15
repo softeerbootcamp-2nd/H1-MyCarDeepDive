e6cf8f141b37fea0217804aafb10e350e53a31b4
shcommit <happysh_s2@naver.com>
2023-08-16 05:42:04 +0900

    feat: QuestionViewSenable 준수

--- GetYa/GetYa/Presentation/CharacterSelect/CharacterDetailSelect/View/CharacterQuestionDetailListView.swift
+++ GetYa/GetYa/Presentation/CharacterSelect/CharacterDetailSelect/View/CharacterQuestionDetailListView.swift
@@ -5,4 +5,11 @@
 //  Created by 양승현 on 2023/08/16.
 //
 
-import Foundation
+import UIKit
+
+final class CharacterQuestionDetailListView: CheckListStackView, QuestionViewSendable {
+    // MARK: - Properties
+    override var selectedItemIndex: Int? {
+        super.selectedItemIndex
+    }
+}
