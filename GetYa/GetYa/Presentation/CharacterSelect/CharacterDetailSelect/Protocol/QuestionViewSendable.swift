1ac49616291d03f5eba76ecf535fc067f3f501d2
shcommit <happysh_s2@naver.com>
2023-08-16 04:36:15 +0900

    feat: QuestionViewSendable 구현

--- GetYa/GetYa/Presentation/CharacterSelect/CharacterDetailSelect/Protocol/QuestionViewSendable.swift
+++ GetYa/GetYa/Presentation/CharacterSelect/CharacterDetailSelect/Protocol/QuestionViewSendable.swift
@@ -5,4 +5,19 @@
 //  Created by 양승현 on 2023/08/16.
 //
 
-import Foundation
+import UIKit
+
+protocol QuestionViewSendable where Self: UIView {
+    var selectedItemIndex: Int? { get }
+    func sendCarMinimumAndMaximumPrice() -> (minimumValue: Int?, maximumValue: Int?)
+}
+
+extension QuestionViewSendable {
+    var selectedItemIndex: Int? {
+        nil
+    }
+    
+    func sendCarMinimumAndMaximumPrice() -> (minimumValue: Int?, maximumValue: Int?) {
+        return (nil, nil)
+    }
+}
