9451376a353c7a4455a0adba8fc273aed2239c86
shcommit <happysh_s2@naver.com>
2023-08-17 00:13:21 +0900

    feat: slider view 구현

--- GetYa/GetYa/Presentation/CharacterSelect/CharacterDetailSelect/View/CharacterQuestionDetailSliderView.swift
+++ GetYa/GetYa/Presentation/CharacterSelect/CharacterDetailSelect/View/CharacterQuestionDetailSliderView.swift
@@ -6,7 +6,87 @@
 //
 
 import UIKit
+import Combine
 
-final class CharacterQuestionDetailSliderView: UIView, QuestionViewSendable {
+final class CharacterQuestionDetailSliderView: UISlider, QuestionViewSendable {
+    enum Constants {
+        static let trackHeight: CGFloat = .toScaledHeight(value: 8)
+        static let thumbHeight: CGFloat = .toScaledHeight(value: 24)
+    }
     
+    // MARK: - UI properties
+    private lazy var thumbView = UIView(frame: .zero).set {
+        $0.backgroundColor = .white
+        $0.layer.borderWidth = 1
+        $0.layer.borderColor = UIColor.GetYaPalette.gray500.cgColor
+        $0.layer.cornerRadius = Constants.thumbHeight/2
+    }
+    
+    private var thumbImage: UIImage {
+        thumbView.frame = CGRect(
+            x: 0,
+            y: Constants.thumbHeight/2,
+            width: Constants.thumbHeight,
+            height: Constants.thumbHeight)
+        return UIGraphicsImageRenderer(bounds: thumbView.bounds).image { graphicsRenderContext in
+            thumbView.layer.render(in: graphicsRenderContext.cgContext)
+        }
+    }
+    // MARK: - Properties
+    private let priceUnit: Float = 300
+    
+    var valueChanged: AnyPublisher<Int, Never> {
+        publisher(for: .valueChanged)
+            .receive(on: DispatchQueue.main)
+            .filter { [weak self] _ in
+                guard let self else { return false }
+                guard value < minimumValue + priceUnit else { return true }
+                setValue(minimumValue + priceUnit, animated: false)
+                return false
+            }.compactMap { [weak self] _ in
+                guard let self else { return nil }
+                let calculatedValue = round((minimumValue - value)/priceUnit)*priceUnit
+                let choicedPrice: Int = Int(abs(calculatedValue)) + Int(minimumValue)
+                return choicedPrice >= Int(maximumValue) ? Int(maximumValue) : choicedPrice
+            }
+            .removeDuplicates()
+            .eraseToAnyPublisher()
+    }
+    
+    // MARK: - Lifecycles
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure(minValue: 4200, maxValue: 6900)
+        configureUI()
+    }
+    
+    convenience init() {
+        self.init(frame: .zero)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        configure(minValue: 4200, maxValue: 6900)
+        configureUI()
+    }
+    
+    // MARK: - Private Functions
+    func configure(minValue: Float, maxValue: Float) {
+        minimumValue = minValue
+        maximumValue = maxValue
+        setValue(minValue + priceUnit, animated: false)
+    }
+    
+    func configureUI() {
+        translatesAutoresizingMaskIntoConstraints = false
+        setThumbImage(thumbImage, for: .normal)
+        minimumTrackTintColor = .GetYaPalette.acriveBlue
+    }
+    
+    // MARK: - Functions
+    override func trackRect(forBounds bounds: CGRect) -> CGRect {
+        var rect = super.trackRect(forBounds: bounds)
+        rect.size.height = Constants.trackHeight
+        return rect
+    }
 }
