ef20aeb898fbe9e8878e7d976033a13adbf6f572
shcommit <happysh_s2@naver.com>
2023-08-16 11:06:30 +0900

    feat: SliderView 클래스 추가

--- GetYa/GetYa/Presentation/CharacterSelect/CharacterDetailSelect/View/CharacterQuestionDetailSliderView.swift
+++ GetYa/GetYa/Presentation/CharacterSelect/CharacterDetailSelect/View/CharacterQuestionDetailSliderView.swift
@@ -1,8 +1,12 @@
 //
-//  CharacterQUestionDetailSliderView.swift
+//  CharacterQuestionDetailSliderView.swift
 //  GetYa
 //
 //  Created by 양승현 on 2023/08/16.
 //
 
-import Foundation
+import UIKit
+
+final class CharacterQuestionDetailSliderView: UIView, QuestionViewSendable {
+    
+}
