1c8f1b150d08ae35c3180a87f14d92403f8328a5
Kwoun Ki Ho <fingercut3822@gmail.com>
2023-08-16 12:50:31 +0900

    feat: cors 허용

---
+++ src/main/java/com/h1/mycardeepdive/global/config/WebConfiguration.java
@@ -0,0 +1,16 @@
+package com.h1.mycardeepdive.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("https://*.make-my-car.shop", "http://*.make-my-car.shop", "http://localhost:5173")
+                .allowedMethods("*");
+    }
+}
