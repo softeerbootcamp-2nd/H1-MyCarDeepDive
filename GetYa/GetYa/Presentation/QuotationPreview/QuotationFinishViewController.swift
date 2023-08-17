37defe1673b2c2b563566844d2925c0469a22f4c
shcommit <happysh_s2@naver.com>
2023-08-17 13:40:12 +0900

    add: QuotationFinishViewController 생성

--- GetYa/GetYa/Presentation/QuotationPreview/QuotationFinishViewController.swift
+++ GetYa/GetYa/Presentation/QuotationPreview/QuotationFinishViewController.swift
@@ -5,4 +5,16 @@
 //  Created by 양승현 on 2023/08/17.
 //
 
-import Foundation
+import UIKit
+
+final class QuotationFinishViewController: BaseViewController {
+    // MARK: - Lifecycles
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        view.backgroundColor = .white
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
