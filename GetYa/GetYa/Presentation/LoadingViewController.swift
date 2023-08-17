1c363cff5403427d3fc618a53c5d4edbb7c2cbeb
NamSeok-Bae <qoskatjr2@gmail.com>
2023-08-17 21:41:08 +0900

    add: LoadingViewController 생성

---
+++ GetYa/GetYa/Presentation/LoadingViewController.swift
@@ -0,0 +1,27 @@
+//
+//  LoadingViewController.swift
+//  GetYa
+//
+//  Created by 배남석 on 2023/08/17.
+//
+
+import UIKit
+
+class LoadingViewController: UIViewController {
+    // MARK: - UI properties
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycles
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    // MARK: - Private Functions
+    
+    // MARK: - Functions
+    
+    // MARK: - Objc Functions
+}
