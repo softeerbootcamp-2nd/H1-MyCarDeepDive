e0de0415c71a93cec22794aad8355dc816d57f09
DaeHee99 <mokday99@gmail.com>
2023-08-17 11:22:00 +0900

    refactor: context 구조 정리

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,7 +4,7 @@ import App from './App.tsx';
 import './index.css';
 
 import { BrowserRouter } from 'react-router-dom';
-import CarProvider from './context/CarProvider.tsx';
+import CarProvider from './context/CarProvider';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
