6079fa0fcfff23828156bf22ce1946cfe3869460
NamSeok-Bae <qoskatjr2@gmail.com>
2023-08-17 22:54:57 +0900

    feat: LoadingViewController 구현

--- GetYa/GetYa/Presentation/LoadingViewController.swift
+++ GetYa/GetYa/Presentation/LoadingViewController.swift
@@ -6,9 +6,45 @@
 //
 
 import UIKit
+import Lottie
 
 class LoadingViewController: UIViewController {
+    enum Constants {
+        enum LottieView {
+            static let height: CGFloat = .toScaledHeight(value: 188)
+        }
+        enum Label {
+            static let topMargin: CGFloat = .toScaledHeight(value: 70)
+        }
+    }
+    
     // MARK: - UI properties
+    private let label: CommonLabel = CommonLabel(
+        fontType: .custom(
+            size: 26,
+            kern: -0.3,
+            lineHeight: 32,
+            nameType: .regularHead),
+        color: .GetYaPalette.gray50,
+        text: "나만의 펠리세이드를\n만들고 있어요"
+    ).set {
+        $0.configurePartTextFont(
+            otherFontType: .custom(
+                size: 26,
+                kern: -0.3,
+                lineHeight: 32,
+                nameType: .boldHead),
+            partText: "펠리세이드")
+        $0.textAlignment = .center
+    }
+    
+    private let lottieView = LottieAnimationView().set {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.animation = LottieAnimation.named("LoadingLottie")
+        $0.animationSpeed = 1.0
+        $0.loopMode = .loop
+        $0.contentMode = .scaleAspectFit
+    }
     
     // MARK: - Properties
     
@@ -16,10 +52,55 @@ class LoadingViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setupViews()
+        configureUI()
+        
+        // TODO: 여기서 네트워크 요청이 완료되면 화면 dismiss 하도록 하기 (Lottie가 제대로 없어지는지 확인 필요)
+        lottieView.play()
     }
     
     // MARK: - Private Functions
+    private func setupViews() {
+        view.addSubviews([
+            label,
+            lottieView
+        ])
+    }
+    
+    private func configureUI() {
+        view.backgroundColor = .white
+        
+        configureNavigationBar()
+        configureLabel()
+        configureLottieView()
+    }
+    
+    private func configureNavigationBar() {
+        navigationItem.title = ""
+        navigationItem.titleView = UIImageView(image: UIImage(named: "Black_Logo"))
+    }
+    
+    private func configureLabel() {
+        typealias Const = Constants.Label
+        
+        NSLayoutConstraint.activate([
+            label.topAnchor.constraint(
+                equalTo: view.safeAreaLayoutGuide.topAnchor,
+                constant: Const.topMargin),
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+    }
+    
+    private func configureLottieView() {
+        typealias Const = Constants.LottieView
+        
+        NSLayoutConstraint.activate([
+            lottieView.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor),
+            lottieView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            lottieView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            lottieView.heightAnchor.constraint(equalToConstant: Const.height)
+        ])
+    }
     
     // MARK: - Functions
     
