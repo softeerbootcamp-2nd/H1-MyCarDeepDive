589b30383d5673ed2cab1cd2b1fad285a3e558f5
shcommit <happysh_s2@naver.com>
2023-08-18 10:40:11 +0900

    feat: default quotation preview view model 임시 구현

--- GetYa/GetYa/Presentation/QuotationPreview/DefaultQuotationPreview/DefaultQuotationPreviewViewModel.swift
+++ GetYa/GetYa/Presentation/QuotationPreview/DefaultQuotationPreview/DefaultQuotationPreviewViewModel.swift
@@ -6,3 +6,73 @@
 //
 
 import Foundation
+import Combine
+
+// MARK: - View Model
+struct DefaultQuotationPreviewMainHeaderModel {
+    var recommendCarProductOption: QuotationPreviewCarInfoModel
+    var firstSectionTitle: String
+}
+
+final class DefaultQuotationPreviewViewModel: CommonQuotationPreviewTableViewModel {
+    // TODO: 서버에서 받아와야 할 데이터
+    // MARK: - Properties
+    private var mainSectionHeader: QuotationPreviewMainHeaderModel
+    private var sectionHeaders: [String]
+    private var secondSectionFooter: String
+    
+    override init() {
+        mainSectionHeader = .mock
+        sectionHeaders = QuotationPreviewHeaderTitleList.lists
+        secondSectionFooter = "48,120,000원"
+        super.init(dataSource: QuotationPreviewCarProductOptionModel.mocks)
+    }
+}
+
+// MARK: - CharacterSelectSuccessViewModelable
+extension DefaultQuotationPreviewViewModel: DetailQuotationPreviewViewModelable {
+    func transform(input: Input) -> Output {
+        return Publishers.MergeMany(
+            customButtonEventChains(input),
+            quickQuoteEventChains(input),
+            viewDidLoadChains(input)
+        ).eraseToAnyPublisher()
+    }
+}
+
+// MARK: - CharacterSelectSuccessViewModelable private function
+private extension DefaultQuotationPreviewViewModel {
+    func customButtonEventChains(_ input: Input) -> Output {
+        return input.customButtonEvent
+            .map { _ -> State in return .gotoCustomPage }
+            .eraseToAnyPublisher()
+    }
+    
+    // 서버에서 받아와서 업데이트?!
+    func quickQuoteEventChains(_ input: Input) -> Output {
+        return input.quickQuoteEvent
+            .map { _ -> State in return .updateRecommendThumbnailKeywords }
+            .eraseToAnyPublisher()
+    }
+    
+    func viewDidLoadChains(_ input: Input) -> Output {
+        return input.viewDidLoad
+            .map { _ -> State in return .none }
+            .eraseToAnyPublisher()
+    }
+}
+
+// MARK: - CharacterSSTableViewAdapterDataSource
+extension DefaultQuotationPreviewViewModel: DetailQuotationPreviewTableViewAdapterDataSource {
+    var secondSectionFooterItem: String {
+        secondSectionFooter
+    }
+    
+    var mainSectionHeaderItem: QuotationPreviewMainHeaderModel {
+        mainSectionHeader
+    }
+    
+    var secondSectionHeaderItem: String {
+        sectionHeaders[1]
+    }
+}
