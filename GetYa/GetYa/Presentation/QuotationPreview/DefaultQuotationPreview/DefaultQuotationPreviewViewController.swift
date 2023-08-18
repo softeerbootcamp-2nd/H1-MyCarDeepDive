3192abdd22539d10f0c992986ba0c9d8847dccfa
shcommit <happysh_s2@naver.com>
2023-08-18 10:39:49 +0900

    feat: Defalut quotation preview view controller 구현

--- GetYa/GetYa/Presentation/QuotationPreview/DefaultQuotationPreview/DefaultQuotationPreviewViewController.swift
+++ GetYa/GetYa/Presentation/QuotationPreview/DefaultQuotationPreview/DefaultQuotationPreviewViewController.swift
@@ -1,8 +1,115 @@
 //
-//  ViewController.swift
+//  DefaultQuotationPreviewViewController.swift
 //  GetYa
 //
 //  Created by 양승현 on 2023/08/18.
 //
 
-import Foundation
+import UIKit
+
+final class DefaultQuotationPreviewViewController: UIViewController {
+    private enum Constants {
+        enum BottomCustomOrQuoteView {
+            static let height: CGFloat = CustomOrQuoteSelectView
+                .Constants.intrinsicContentHeight
+        }
+    }
+    
+    // MARK: - UI Properties
+    private let tableView = DefaultQuotationPreviewTableView(frame: .zero, style: .grouped)
+    private let bottomCustomOrQuoteView = CustomOrQuoteSelectView()
+    
+    // MARK: - Properties
+    private var adapter: DetailQuotationPreviewTableViewAdapter!
+    private var viewModel: (
+        any DetailQuotationPreviewViewModelable
+        & DetailQuotationPreviewTableViewAdapterDataSource
+        & CommonQuotationPreviewTableViewAdapterDataSource)!
+    
+    // MARK: - Lifecycles
+    init(
+        viewModel: some DetailQuotationPreviewViewModelable
+        & DetailQuotationPreviewTableViewAdapterDataSource
+        & CommonQuotationPreviewTableViewAdapterDataSource
+    ) {
+        super.init(nibName: nil, bundle: nil)
+        self.viewModel = viewModel
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        viewModel = DetailQuotationPreviewViewModel()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+        adapter = .init(
+            tableView: tableView,
+            dataSource: viewModel)
+        
+    }
+    
+    // MARK: - Functions
+    func configureUI() {
+        view.backgroundColor = .white
+        setupUI()
+        configureNavigationBar()
+    }
+    
+    // MARK: - Private functions
+    private func configureNavigationBar() {
+        let image = UIImage(named: "Black_Logo")
+        self.navigationItem.title = ""
+        self.navigationItem.titleView = UIImageView(image: image)
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(
+            image: UIImage(systemName: "arrow.left")?.withTintColor(
+                .GetYaPalette.gray0,
+                renderingMode: .alwaysOriginal),
+            style: .plain,
+            target: self,
+            action: #selector(didTapNavigationBackButton))
+    }
+    
+    // MARK: - @Objc functions
+    @objc func didTapNavigationBackButton() {
+        navigationController?.popViewController(animated: true)
+    }
+}
+
+// MARK: - LayoutSupportable
+extension DefaultQuotationPreviewViewController: LayoutSupportable {
+    func setupViews() {
+        view.addSubviews([
+            tableView,
+            bottomCustomOrQuoteView])
+    }
+    
+    func setupConstriants() {
+        configureTableViewConstriants()
+        configureBottomCustomOrQuoteView()
+    }
+    
+    // MARK: - LayoutSupportable private helper
+    private func configureTableViewConstriants() {
+        _=tableView.set {
+            NSLayoutConstraint.activate([
+                $0.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+                $0.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+                $0.trailingAnchor.constraint(equalTo: view.trailingAnchor)])
+        }
+    }
+    
+    private func configureBottomCustomOrQuoteView() {
+        _=bottomCustomOrQuoteView.set {
+            NSLayoutConstraint.activate([
+                $0.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+                $0.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+                $0.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+                $0.heightAnchor.constraint(equalToConstant: Constants.BottomCustomOrQuoteView.height),
+                $0.topAnchor.constraint(
+                    equalTo: tableView.bottomAnchor,
+                    constant: -CustomOrQuoteSelectView.Constants.gradientLayerHeight)])
+        }
+    }
+}
