24580fb251c7df437e9fad5be703b71daa732857
NamSeok-Bae <qoskatjr2@gmail.com>
2023-08-19 22:18:51 +0900

    feat: OptionSelect Segmented Control 구현

--- GetYa/GetYa/Presentation/CarSettingSelect/OptionSelect/OptionSelectViewController.swift
+++ GetYa/GetYa/Presentation/CarSettingSelect/OptionSelect/OptionSelectViewController.swift
@@ -8,21 +8,64 @@
 import UIKit
 
 class OptionSelectViewController: UIViewController {
+    enum Constants {
+        enum SegmentedControl {
+            static let height: CGFloat = .toScaledHeight(value: 52)
+        }
+    }
+    
     // MARK: - UI properties
+    private lazy var segmentedControl = OptionSelectSegmentedControl(items: ["추가 옵션", "기본 포함 옵션"]).set {
+        $0.addTarget(self, action: #selector(changeSegmentedValue), for: .valueChanged)
+    }
     
     // MARK: - Properties
+    private var currentSegmentedIndex: Int = 0 {
+        didSet {
+            print(currentSegmentedIndex)
+        }
+    }
     
     // MARK: - Lifecycles
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .white
+        setupViews()
+        configureUI()
+    }
+    
+    override func viewSafeAreaInsetsDidChange() {
+        super.viewSafeAreaInsetsDidChange()
+        configureSegmentedControl()
     }
     
     // MARK: - Private Functions
+    private func setupViews() {
+        view.addSubviews([
+            segmentedControl
+        ])
+    }
+    
+    private func configureUI() {
+        view.backgroundColor = .white
+        
+    }
+    
+    private func configureSegmentedControl() {
+        let safeLayoutFrame = view.safeAreaLayoutGuide.layoutFrame
+        segmentedControl.frame = CGRect(
+            x: safeLayoutFrame.minX,
+            y: safeLayoutFrame.minY,
+            width: safeLayoutFrame.width,
+            height: Constants.SegmentedControl.height)
+        segmentedControl.configureBottomBorder()
+    }
     
     // MARK: - Functions
     
     // MARK: - Objc Functions
+    @objc private func changeSegmentedValue(sender: UISegmentedControl) {
+        currentSegmentedIndex = sender.selectedSegmentIndex
+    }
 
 }
