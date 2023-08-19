0c8e3c53dceb033b68dc85c56a9881c1fbc17629
shcommit <happysh_s2@naver.com>
2023-08-20 02:19:41 +0900

    feat: request(with:) 구현, checkResult(data:response:), decode(data:) 구현

--- GetYa/GetYa/Util/Service/Provider/SessionProviderImpl.swift
+++ GetYa/GetYa/Util/Service/Provider/SessionProviderImpl.swift
@@ -6,3 +6,54 @@
 //
 
 import Foundation
+
+final class SessionProviderImpl {
+    private let session: URLSession
+    init(session: URLSession = .shared) {
+        self.session = session
+    }
+}
+
+// MARK: - EndpointProvider
+extension SessionProviderImpl: EndpointProvider {
+    func request<R, E>(
+        with endpoint: E
+    ) async throws -> R where R == E.ResponseDTO, E: NetworkInteractionable {
+        let urlRequest = try endpoint.makeRequest()
+        do {
+            let (data, _) = try await session.data(for: urlRequest)
+            return try decode(data: data)
+        } catch let networkError as NetworkError {
+            throw networkError
+        } catch {
+            throw NetworkError.urlRequest(error)
+        }
+    }
+    
+    func request(_ url: URL) async throws -> Data {
+        return .init()
+    }
+}
+
+// MARK: - Private functions
+extension SessionProviderImpl {
+    private func checkResult(data: Data, _ response: URLResponse) throws {
+        guard let response = response as? HTTPURLResponse else {
+            throw NetworkError.unknown
+        }
+        guard response.responseType == .success else {
+            throw NetworkError.invalidResponseStatus(response.responseType)
+        }
+        guard !data.isEmpty else { throw NetworkError.emptyBytes }
+    }
+    
+    private func decode<T: Decodable>(data: Data) throws -> T {
+        do {
+            let decoder = JSONDecoder()
+            let model = try decoder.decode(T.self, from: data)
+            return model
+        } catch {
+            throw NetworkError.emptyBytes
+        }
+    }
+}
