f4ba7f6b9a4810f8ef5ea601ef73b21bd47efe5c
shcommit <happysh_s2@naver.com>
2023-08-20 00:50:22 +0900

    feat: Endpoint 정의

--- GetYa/GetYa/Util/Service/Endpoint/Endpoint.swift
+++ GetYa/GetYa/Util/Service/Endpoint/Endpoint.swift
@@ -5,3 +5,35 @@
 //  Created by 양승현 on 2023/08/19.
 //
 
+final class Endpoint<ResponseModel>: NetworkInteractionable where ResponseModel: Decodable {
+    typealias ResponseDTO = ResponseModel
+    
+    var scheme: String
+    var host: String
+    var method: HTTPMethod
+    var prefixPath: String
+    var path: String
+    var queryParams: Encodable?
+    var bodyParams: Encodable?
+    var headers: [String: String]?
+    
+    init(
+        scheme: String = "http://",
+        host: String,
+        method: HTTPMethod,
+        prefixPath: String = "/api/v1",
+        path: String,
+        queryParams: Encodable? = nil,
+        bodyParams: Encodable? = nil,
+        headers: [String: String]? = nil
+    ) {
+        self.scheme = scheme
+        self.host = host
+        self.method = method
+        self.prefixPath = prefixPath
+        self.path = path
+        self.queryParams = queryParams
+        self.bodyParams = bodyParams
+        self.headers = headers
+    }
+}
