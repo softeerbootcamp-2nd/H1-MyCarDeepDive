3bed7ed66e165a8fb76116bdd3c1fed2c5a6d5d6
NamSeok-Bae <qoskatjr2@gmail.com>
2023-08-19 21:59:43 +0900

    add: OptionSelectSegmentedControl 생성

---
+++ GetYa/GetYa/Presentation/CarSettingSelect/OptionSelect/OptionSelectSegmentedControl.swift
@@ -0,0 +1,20 @@
+//
+//  OptionSelectSegmentedControl.swift
+//  GetYa
+//
+//  Created by 배남석 on 2023/08/19.
+//
+
+import UIKit
+
+class OptionSelectSegmentedControl: UISegmentedControl {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
