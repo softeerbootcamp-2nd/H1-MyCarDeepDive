a58460c95990970e5d4c910b86e25260e7ab2014
shcommit <happysh_s2@naver.com>
2023-08-19 23:38:04 +0900

    feat: HttpStatus 추가

--- GetYa/GetYa/Util/Service/Common/HttpStatus.swift
+++ GetYa/GetYa/Util/Service/Common/HttpStatus.swift
@@ -5,4 +5,37 @@
 //  Created by 양승현 on 2023/08/19.
 //
 
-import Foundation
+struct HttpStatus {
+    var statusCode: Int
+    
+    enum ResponseType {
+        /// 1xx
+        case ing
+        
+        case undefined
+        
+        /// 2xx
+        case success
+        
+        /// 4xx
+        case iOSError
+        
+        /// 5xx
+        case serverError
+    }
+    
+    var responseType: ResponseType {
+        switch statusCode {
+        case 100...199:
+            return .ing
+        case 200...299:
+            return .success
+        case 400...499:
+            return .iOSError
+        case 500...599:
+            return .serverError
+        default:
+            return .undefined
+        }
+    }
+}
