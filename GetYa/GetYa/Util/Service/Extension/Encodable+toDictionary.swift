09786943c7714fe0a92281ab7419583b4a3eb0b2
shcommit <happysh_s2@naver.com>
2023-08-19 23:55:36 +0900

    feat: Encodable -> [String: Any]로 변경하는 computed property 추가

--- GetYa/GetYa/Util/Service/Extension/Encodable+toDictionary.swift
+++ GetYa/GetYa/Util/Service/Extension/Encodable+toDictionary.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+import OSLog
+
+extension Encodable {
+    var toDictionary: [String: Any]? {
+        do {
+            let jsonData = try JSONEncoder().encode(self)
+            return try JSONSerialization
+                .jsonObject(
+                    with: jsonData,
+                    options: []
+                ) as? [String: Any]
+        } catch {
+            os_log("Failed to convert encodable json data as [String: Any]", type: .error)
+        }
+    }
+}
