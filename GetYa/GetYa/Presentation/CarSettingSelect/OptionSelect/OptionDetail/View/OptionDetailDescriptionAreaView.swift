dfc1bc07e14365e3d2cd4c9122c6d69230460bf9
shcommit <happysh_s2@naver.com>
2023-08-20 20:17:43 +0900

    add: OptionDetailDescriptionAreaVie 생성

---
+++ GetYa/GetYa/Presentation/CarSettingSelect/OptionSelect/OptionDetail/View/OptionDetailDescriptionAreaView.swift
@@ -0,0 +1,17 @@
+//
+//  OptionDefailDescriptionAreaView.swift
+//  GetYa
+//
+//  Created by 양승현 on 2023/08/20.
+//
+
+import UIKit
+
+final class OptionDefailDescriptionAreaView: UIView {
+    // MARK: - UI properties
+    // MARK: - Properties
+    // MARK: - Lifecycles
+    // MARK: - Private Functions
+    // MARK: - Functions
+    // MARK: - Objc Functions
+}
