f9900270ee138e6b718a6858258b82599cf63145
shcommit <happysh_s2@naver.com>
2023-08-21 05:53:40 +0900

    feat: 외부에서 데이터 주입 관련 로직 구현

--- GetYa/GetYa/Presentation/CarSettingSelect/OptionSelect/OptionDetail/View/OptionPackageDescriptionView.swift
+++ GetYa/GetYa/Presentation/CarSettingSelect/OptionSelect/OptionDetail/View/OptionPackageDescriptionView.swift
@@ -20,7 +20,7 @@ final class OptionPackageDescriptionView: UIView {
             static let height: CGFloat = .toScaledHeight(value: 94)
         }
         
-        enum PageSegmentView {
+        enum PageControl {
             static let topMargin: CGFloat = .toScaledHeight(value: 28)
             static let height: CGFloat = .toScaledWidth(value: 8)
             static let width = height
@@ -28,7 +28,18 @@ final class OptionPackageDescriptionView: UIView {
         
     }
     // MARK: - UI properties
+    private let optionPackageLabel = CommonLabel(
+        fontType: .mediumCaption1,
+        color: .GetYaPalette.gray400,
+        text: "")
+    private let optionDetailDescriptionView = OptionDetailDescriptionAreaView(frame: .zero)
+    private let optionTitleCollectionView = UICollectionView(
+        frame: .zero,
+        collectionViewLayout: UICollectionViewFlowLayout())
+    private var pageControl = CommonPageControl(numberOfPages: 0)
+    
     // MARK: - Properties
+
     // MARK: - Lifecycles
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -39,6 +50,26 @@ final class OptionPackageDescriptionView: UIView {
     }
     
     // MARK: - Private Functions
+    private func setPageControlSelectedPage(currentPage: Int) {
+        pageControl.currentPage = currentPage
+    }
+    
     // MARK: - Functions
+    func configure(
+        delegator: (UICollectionViewDataSource & UICollectionViewDelegate)?,
+        pages: Int?
+    ) {
+        setOptionTitleCollectionView(from: delegator)
+        setPageCount(with: pages)
+    }
+    
+    func setOptionTitleCollectionView(from delegator: (UICollectionViewDataSource & UICollectionViewDelegate)?) {
+        optionTitleCollectionView.delegate = delegator
+        optionTitleCollectionView.dataSource = delegator
+    }
+    
+    func setPageCount(with pages: Int?) {
+        pageControl.numberOfPages = pages ?? 0
+    }
     // MARK: - Objc Functions
 }
