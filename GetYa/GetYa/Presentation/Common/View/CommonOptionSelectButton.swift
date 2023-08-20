2a3d0e8ff8619b84df5c5c66524b0d86f806a5ad
shcommit <happysh_s2@naver.com>
2023-08-20 16:24:08 +0900

    feat: CommonOptionSelectButton 선택, 미선택일 때 색 변경, 내장 titleLabel, ImageView spacing안되서 subview로 대체

--- GetYa/GetYa/Presentation/Common/View/CommonOptionSelectButton.swift
+++ GetYa/GetYa/Presentation/Common/View/CommonOptionSelectButton.swift
@@ -8,12 +8,52 @@
 import UIKit
 
 class CommonOptionSelectButton: UIButton {
+    enum Constants {
+        static let height: CGFloat = .toScaledHeight(value: 28)
+        enum IconImageView {
+            static let leadingMargin: CGFloat = .toScaledWidth(value: 14)
+            static let topMargin: CGFloat = .toScaledHeight(value: 6)
+            static let bottomMargin: CGFloat = .toScaledHeight(value: -6)
+            static let width: CGFloat = .toScaledWidth(value: 13)
+            static let height = width
+        }
+        enum IconTitleLabel {
+            static let leadingMargin: CGFloat = .toScaledWidth(value: 8)
+            static let trailingMargin: CGFloat = .toScaledWidth(value: -14)
+            static let size: CGSize = {
+                let label = CommonLabel.init(fontType: .mediumCaption1, color: .white, text: "선택")
+                return label.intrinsicContentSize
+            }()
+        }
+    }
+
     // MARK: - UI Properties
+    private let resizedImage: UIImage? = {
+        let image = UIImage(named: "Blue-Check-Circle")
+        let preferSize: CGSize = .init(
+            width: .toScaledWidth(value: 16),
+            height: .toScaledWidth(value: 16))
+        return image?.resize(targetSize: preferSize)
+    }()
+    private lazy var iconImageView = UIImageView(image: resizedImage).set {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.contentMode = .scaleAspectFit
+    }
+    private let iconTitleLabel = CommonLabel.init(
+        fontType: .mediumCaption1,
+        color: .GetYaPalette.primary,
+        text: "선택"
+    ).set {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+    }
     
     // MARK: - Properties
+    override var intrinsicContentSize: CGSize {
+        return .init(width: .toScaledWidth(value: 69), height: Constants.height)
+    }
     override var isSelected: Bool {
         didSet {
-            self.layer.backgroundColor = isSelected ? UIColor(hexString: "00428E").cgColor : UIColor.white.cgColor
+            isSelected ? configureSelectedState() : configureDeselectedState()
         }
     }
     
@@ -32,17 +72,80 @@ class CommonOptionSelectButton: UIButton {
         configureUI()
     }
     
-    // MARK: - Functions
+    // MARK: - Private functions
     private func configureUI() {
-        self.translatesAutoresizingMaskIntoConstraints = false
-        self.setTitle("선택", for: .normal)
-        self.setImage(UIImage(named: "Blue-Check-Circle"), for: .normal)
-        self.setImage(UIImage(named: "White-Check-Circle"), for: .selected)
-        self.setTitleColor(UIColor(hexString: "00428E"), for: .normal)
-        self.setTitleColor(.white, for: .selected)
-        self.layer.borderWidth = 1
-        self.layer.borderColor = UIColor(hexString: "00428E").cgColor
-        self.layer.cornerRadius = 20
-        self.layer.backgroundColor = UIColor.white.cgColor
+        translatesAutoresizingMaskIntoConstraints = false
+        layer.cornerRadius = Constants.height/2
+        layer.borderWidth = 1
+        layer.borderColor = UIColor.GetYaPalette.primary.cgColor
+        backgroundColor = .white
+        setupUI()
+    }
+    
+    private func configureSelectedState() {
+        UIView.animate(
+            withDuration: 0.15,
+            delay: 0,
+            options: [.curveEaseInOut]
+        ) {
+            let color: UIColor = .white
+            self.iconImageView.image = self.resizedImage?.withTintColor(
+                color,
+                renderingMode: .alwaysOriginal)
+            self.iconTitleLabel.textColor = color
+            self.backgroundColor = .GetYaPalette.primary
+        }
+    }
+    
+    private func configureDeselectedState() {
+        UIView.animate(
+            withDuration: 0.15,
+            delay: 0,
+            options: [.curveEaseInOut]
+        ) {
+            let primary: UIColor = .GetYaPalette.primary
+            self.iconImageView.image = self.resizedImage?.withTintColor(
+                primary,
+                renderingMode: .alwaysOriginal)
+            self.iconTitleLabel.textColor = primary
+            self.backgroundColor = .white
+        }
+    }
+}
+
+// MARK: - LayoutSupportable
+extension CommonOptionSelectButton: LayoutSupportable {
+    func setupViews() {
+        addSubviews([
+            iconImageView,
+            iconTitleLabel])
+    }
+    
+    func setupConstriants() {
+        configureIconImageView()
+        configureIconTitleLabel()
+    }
+    
+    // MARK: - LayoutSupport private function
+    private func configureIconImageView() {
+        typealias Const = Constants.IconImageView
+        NSLayoutConstraint.activate([
+            iconImageView.leadingAnchor.constraint(
+                equalTo: leadingAnchor,
+                constant: Const.leadingMargin),
+            iconImageView.centerYAnchor.constraint(equalTo: centerYAnchor)])
+    }
+    
+    private func configureIconTitleLabel() {
+        typealias Const = Constants.IconTitleLabel
+        NSLayoutConstraint.activate([
+            iconTitleLabel.leadingAnchor.constraint(
+                equalTo: iconImageView.trailingAnchor,
+                constant: Const.leadingMargin),
+            iconTitleLabel.trailingAnchor.constraint(
+                equalTo: trailingAnchor,
+                constant: Const.trailingMargin),
+            iconTitleLabel.centerYAnchor.constraint(
+                equalTo: centerYAnchor)])
     }
 }
