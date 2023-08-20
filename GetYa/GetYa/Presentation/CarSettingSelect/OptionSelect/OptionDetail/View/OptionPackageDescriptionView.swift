ff95985c160d1fed85062c2907283352409e7687
shcommit <happysh_s2@naver.com>
2023-08-21 05:28:53 +0900

    feat: OptionPackageDescriptionView UI subview's constraints 추가

--- GetYa/GetYa/Presentation/CarSettingSelect/OptionSelect/OptionDetail/View/OptionPackageDescriptionView.swift
+++ GetYa/GetYa/Presentation/CarSettingSelect/OptionSelect/OptionDetail/View/OptionPackageDescriptionView.swift
@@ -5,4 +5,40 @@
 //  Created by 양승현 on 2023/08/21.
 //
 
-import Foundation
+import UIKit
+
+final class OptionPackageDescriptionView: UIView {
+    enum Constants {
+        enum OptionDetailDescriptionView {
+            static let topMargin: CGFloat = .toScaledHeight(value: 4)
+            static let maximumHeight: CGFloat = OptionDetailDescriptionAreaView
+                .Constants
+                .maximumHeight
+        }
+        enum OptionTitleCollectionView {
+            static let topMargin: CGFloat = .toScaledHeight(value: 28)
+            static let height: CGFloat = .toScaledHeight(value: 94)
+        }
+        
+        enum PageSegmentView {
+            static let topMargin: CGFloat = .toScaledHeight(value: 28)
+            static let height: CGFloat = .toScaledWidth(value: 8)
+            static let width = height
+        }
+        
+    }
+    // MARK: - UI properties
+    // MARK: - Properties
+    // MARK: - Lifecycles
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    // MARK: - Private Functions
+    // MARK: - Functions
+    // MARK: - Objc Functions
+}
