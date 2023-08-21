bf0dcc2846a5e297caf54ce7a97da09d856da3f2
shcommit <happysh_s2@naver.com>
2023-08-21 20:10:07 +0900

    chore: bodyParam error 삭제

--- GetYa/GetYa/Util/Service/Common/NetworkError.swift
+++ GetYa/GetYa/Util/Service/Common/NetworkError.swift
@@ -11,7 +11,6 @@ enum NetworkError: Swift.Error {
     /// Base Url 만들 때 
     case components
     case unknown
-    case invalidBodyParams
     case urlRequest(Error)
     case invalidResponseStatus(HTTPURLResponse.ResponseType)
     case timeout
