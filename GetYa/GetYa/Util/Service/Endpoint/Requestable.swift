bae0fdcaa89de4adba6faabd7673fc4fc9106869
shcommit <happysh_s2@naver.com>
2023-08-21 20:10:58 +0900

    feat: Requestable 함수 수정. responseType로 특정 path 값 받는거 문제 해결!!

--- GetYa/GetYa/Util/Service/Endpoint/Requestable.swift
+++ GetYa/GetYa/Util/Service/Endpoint/Requestable.swift
@@ -12,23 +12,39 @@ protocol Requestable {
     var host: String { get }
     var method: HTTPMethod { get }
     var prefixPath: String { get }
-    var midPath: String { get }
+    var responseType: ResponseType { get }
+    var queryParams: Encodable? { get }
     var headers: [String: String]? { get }
 }
 
 // MARK: - Functions
 extension Requestable {
-    func url(with path: String) throws -> URL {
-        let baseUrlString = "\(scheme)://\(host)" + prefixPath + "/(path)"
-        guard let url = URLComponents(string: baseUrlString)?.url else {
+    func url(with responseType: ResponseType) throws -> URL {
+        var baseUrlString = "\(scheme)://\(host)" + prefixPath + responseType.path
+        guard var components = URLComponents(string: baseUrlString) else {
             throw NetworkError.components
         }
+        components.queryItems = makeQueryItems
+        guard let url = components.url else {
+            throw NetworkError.components
+        }
+        print(url)
         return url
     }
     
-    func makeRequest(with path: String) throws -> URLRequest {
-        var urlRequest = URLRequest(url: try url(with: path))
+    func makeRequest(with responseType: ResponseType) throws -> URLRequest {
+        var urlRequest = URLRequest(url: try url(with: responseType))
         _=headers?.map { urlRequest.setValue($1, forHTTPHeaderField: $0) }
         return urlRequest
+    }
+}
+
+// MARK: Private functions
+private extension Requestable {
+    var makeQueryItems: [URLQueryItem]? {
+        guard let querys = queryParams?.toDictionary else {
+            return nil
+        }
+        return querys.map { URLQueryItem(name: $0.key, value: "\($0.value)") }
     }
 }
