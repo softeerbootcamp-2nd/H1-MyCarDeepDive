c85d02c3df253b45a31ae9a4102e9b64ae8fab56
shcommit <happysh_s2@naver.com>
2023-08-21 20:05:42 +0900

    feat: request<R,E>(endpoint:) 함수 추가

--- GetYa/GetYa/Util/Service/Protocol/EndpointProvider.swift
+++ GetYa/GetYa/Util/Service/Protocol/EndpointProvider.swift
@@ -8,6 +8,9 @@
 import Foundation
 
 protocol EndpointProvider: AnyObject {
-    func request<R: Decodable, E: Endpoint<R>>(with path: String, with endpoint: E) async throws -> R
+    func request<R: Decodable, E: NetworkInteractionable>(
+        endpoint: E
+    ) async throws -> R where E.ResponseDTO == R
+    
     func request(with url: URL) async throws -> Data 
 }
