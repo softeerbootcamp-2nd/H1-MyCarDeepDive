5cc47340d54499e78eb2a7cde0936fd0b658610e
DaeHee99 <mokday99@gmail.com>
2023-08-21 17:00:48 +0900

    feat: 헤더 로고 홈 바로가기

--- frontend/src/Pages/MyCarResultPage/Head/index.tsx
+++ frontend/src/Pages/MyCarResultPage/Head/index.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from 'react-router-dom';
 import Card from './Card';
 import Phrases from './Phrases';
 import Logo from '@/assets/icon/logo.svg';
@@ -7,9 +8,16 @@ interface Props {
 }
 
 function Head({ setShowShareModal }: Props) {
+  const navigation = useNavigate();
+
   return (
     <div className='w-full h-[506px] bg-[#E6EAEF] relative'>
-      <img src={Logo} alt='hyundai-logo' className='absolute top-8 left-10' />
+      <img
+        src={Logo}
+        alt='hyundai-logo'
+        className='absolute top-8 left-10 cursor-pointer'
+        onClick={() => navigation('/')}
+      />
       <div className='absolute top-[89px] left-1/2 -translate-x-1/2 font-h1-medium'>
         <Phrases />
         <Card setShowShareModal={setShowShareModal} />
