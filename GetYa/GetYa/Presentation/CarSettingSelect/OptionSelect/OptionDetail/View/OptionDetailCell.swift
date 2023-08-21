c8fb0a5a70d2eb2dbbb4d2fc208b6c85589f401a
shcommit <happysh_s2@naver.com>
2023-08-21 10:08:59 +0900

    feat: 기본 option detail cell 생성

--- GetYa/GetYa/Presentation/CarSettingSelect/OptionSelect/OptionDetail/View/OptionDetailCell.swift
+++ GetYa/GetYa/Presentation/CarSettingSelect/OptionSelect/OptionDetail/View/OptionDetailCell.swift
@@ -5,4 +5,44 @@
 //  Created by 양승현 on 2023/08/20.
 //
 
-import Foundation
+import UIKit
+
+final class OptionDetailCell: UICollectionViewCell {
+    static let identifier: String = "OptionDetailCell"
+    // MARK: - UI properties
+    // MARK: - Properties
+    // MARK: - Lifecycles
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    // MARK: - Private Functions
+    private func configureUI() {
+        
+    }
+    
+    // MARK: - Functions
+    
+    // MARK: - Objc Functions
+}
+
+
+// MARK: - UICollectionViewDataSource
+extension OptionDetailCell: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+}
+
+// MARK: - UICollectionViewDelegate
+extension OptionDetailCell: UICollectionViewDelegate {
+    
+}
