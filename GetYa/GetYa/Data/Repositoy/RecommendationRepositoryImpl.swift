80741c60611eea72d1e64444edee05e6896b284b
shcommit <happysh_s2@naver.com>
2023-08-21 23:40:10 +0900

    feat: func fetchCarDetailRecommendation(with:) 추가

--- GetYa/GetYa/Data/Repositoy/RecommendationRepositoryImpl.swift
+++ GetYa/GetYa/Data/Repositoy/RecommendationRepositoryImpl.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+struct RecommendationRepositoryImpl: RecommendationRepository {
+    typealias Endpoints = RecommendationEndpoints
+    let provider = SessionProvider()
+    
+    func fetchCarDetailRecommendation(
+        with requestDTO: CustomRecomendation
+    ) async throws -> QuotationDTO.Data {
+        let endpoint = Endpoints.shared.fetchCarDetailRecommendationOption(with: requestDTO)
+        let responseDTO = try await provider.request(endpoint: endpoint)
+        return responseDTO.data
+    }
+}
