95e407cd1d5c1f04ab22bf699b2757d8405a39cc
shcommit <happysh_s2@naver.com>
2023-08-22 16:25:32 +0900

    feat: QuotationDTO API 변동에 다른 재구현

--- GetYa/GetYa/Data/DTO/QuotationDTO.swift
+++ GetYa/GetYa/Data/DTO/QuotationDTO.swift
@@ -8,81 +8,85 @@
 import Foundation
 
 struct QuotationDTO: Decodable {
-    struct Data: Decodable {
-        let trimName: String
-        let carImgURL: URL
-        let comment1: String
-        let comment2: String
-        let keyword: String
-        let engineName: String
-        let drivingSystemName: String
-        let bodyName: String
-        let carSpecID: Int
-        let trimID: Int
-        let exteriorColorID: Int
-        let exteriorColorName: String
-        let exteriorColorPrice: Int
-        let exteriorColorComment: String
-        let exteriorColorIconURL: URL
-        let interiorColorID: Int
-        let interiorColorName: String
-        let interiorColorPrice: Int
-        let interiorColorComment: String
-        let interiorColorIconURL: URL
-        let options: [Option]
-        let packages: [Package]
-        let totalPrice: Int
-        
-        enum CodingKeys: String, CodingKey {
-            case trimName = "trim_name"
-            case carImgURL = "car_img_url"
-            case comment1, comment2, keyword, engineName, drivingSystemName, bodyName
-            case carSpecID = "car_spec_id"
-            case trimID = "trim_id"
-            case exteriorColorID = "exterior_color_id"
-            case exteriorColorName = "exterior_color_name"
-            case exteriorColorPrice = "exterior_color_price"
-            case exteriorColorComment = "exterior_color_comment"
-            case exteriorColorIconURL = "exterior_color_icon_url"
-            case interiorColorID = "interior_color_id"
-            case interiorColorName = "interior_color_name"
-            case interiorColorPrice = "interior_color_price"
-            case interiorColorComment = "interior_color_comment"
-            case interiorColorIconURL = "interior_color_icon_url"
-            case options, packages, totalPrice
-        }
-    }
+    let trimName: String
+    let carImgUrl: String
+    let comment1: String
+    let comment2: String
+    let keyword: String
+    let engineName: String
+    let drivingSystemName: String
+    let bodyName: String
+    let carSpecId: Int
+    let trimId: Int
+    let exteriorColor: ColorInfoDTO
+    let interiorColor: ColorInfoDTO
+    let options: [OptionInfoDTO]
+    let packages: [PackageInfoDTO]
+    let totalPrice: Int
     
-    struct Option: Decodable {
-        let optionID: Int
-        let optionName: String
-        let optionImgURL: URL
-        let price: Int
-        let comment: String
-        
-        enum CodingKeys: String, CodingKey {
-            case optionID = "option_id"
-            case optionName = "option_name"
-            case optionImgURL = "option_img_url"
-            case price, comment
-        }
+    enum CodingKeys: String, CodingKey {
+        case trimName = "trim_name"
+        case carImgUrl = "car_img_url"
+        case comment1
+        case comment2
+        case keyword
+        case engineName = "engine_name"
+        case drivingSystemName = "driving_system_name"
+        case bodyName = "body_name"
+        case carSpecId = "car_spec_id"
+        case trimId = "trim_id"
+        case exteriorColor = "exterior_color"
+        case interiorColor = "interior_color"
+        case options
+        case packages
+        case totalPrice
     }
-    
-    struct Package: Decodable {
-        let optionID: Int
-        let optionName: String
-        let optionImgURL: URL
-        let price: Int
-        let comment: String
-        
-        enum CodingKeys: String, CodingKey {
-            case optionID = "option_id"
-            case optionName = "option_name"
-            case optionImgURL = "option_img_url"
-            case price, comment
-        }
-    }
+}
 
-    let status: Status
-    let data: Data
+struct ColorInfoDTO: Decodable {
+    let colorId: Int
+    let colorName: String
+    let colorPrice: Int
+    let colorComment: String
+    let colorIconUrl: String
+    
+    enum CodingKeys: String, CodingKey {
+        case colorId = "color_id"
+        case colorName = "color_name"
+        case colorPrice = "color_price"
+        case colorComment = "color_comment"
+        case colorIconUrl = "color_icon_url"
+    }
+}
+
+struct OptionInfoDTO: Decodable {
+    let optionId: Int
+    let optionName: String
+    let optionImgUrl: String
+    let price: Int
+    let comment: String
+    
+    enum CodingKeys: String, CodingKey {
+        case optionId = "option_id"
+        case optionName = "option_name"
+        case optionImgUrl = "option_img_url"
+        case price
+        case comment
+    }
+}
+
+struct PackageInfoDTO: Decodable {
+    let optionId: Int
+    let optionName: String
+    let optionImgUrl: String
+    let price: Int
+    let comment: String
+    
+    enum CodingKeys: String, CodingKey {
+        case optionId = "option_id"
+        case optionName = "option_name"
+        case optionImgUrl = "option_img_url"
+        case price
+        case comment
+    }
 }
