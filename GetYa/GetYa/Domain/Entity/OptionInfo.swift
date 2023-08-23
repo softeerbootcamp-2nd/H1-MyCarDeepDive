038c975e157fcc13fe80eed5f72ebc7bfc0cca26
shcommit <happysh_s2@naver.com>
2023-08-23 16:43:10 +0900

    feat: OptionInfo 추가

---
+++ GetYa/GetYa/Domain/Entity/OptionInfo.swift
@@ -0,0 +1,16 @@
+//
+//  OptionInfo.swift
+//  GetYa
+//
+//  Created by 양승현 on 2023/08/23.
+//
+
+import Foundation
+
+struct OptionInfo {
+    let optionId: Int
+    let price: Int
+    let optionName: String
+    let optionImgUrl: String
+    let comment: String
+}
