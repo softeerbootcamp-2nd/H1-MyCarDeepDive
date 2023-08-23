4f7a904ec95eb6d080976dc6094e859e1207dfc6
shcommit <happysh_s2@naver.com>
2023-08-23 17:03:51 +0900

    feat: CustomRecommendationModel 추가

---
+++ GetYa/GetYa/Domain/Entity/CustomRecomendationModel.swift
@@ -0,0 +1,14 @@
+//
+//  CustomRecomendationModel.swift
+//  GetYa
+//
+//  Created by 양승현 on 2023/08/23.
+//
+
+struct CustomRecomendationModel: Encodable {
+    let drivingExperienceId: Int
+    let familyMembersId: Int
+    let carPurposeId: Int
+    let personalValueId: Int
+    let maxBudget: Int
+}
