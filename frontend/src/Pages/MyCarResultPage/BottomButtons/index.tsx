8e60e22dc4a9838374bd54cfeda14e589e9d9082
DaeHee99 <mokday99@gmail.com>
2023-08-23 14:56:16 +0900

    feat: 결과 페이지에서 페이지 이동

--- frontend/src/Pages/MyCarResultPage/BottomButtons/index.tsx
+++ frontend/src/Pages/MyCarResultPage/BottomButtons/index.tsx
@@ -1,6 +1,9 @@
 import Button from '@/Components/Button';
+import { useNavigate } from 'react-router-dom';
 
 function BottomButtons() {
+  const navigation = useNavigate();
+
   return (
     <div className='flex gap-4 justify-between mb-9'>
       <Button
@@ -8,14 +11,19 @@ function BottomButtons() {
         height='h-[52px]'
         variant='secondary'
         text='수정'
-        onClick={() => console.log('수정')}
+        onClick={() => navigation(-1)}
       />
       <Button
         width='w-[296px]'
         height='h-[52px]'
         variant='primary'
         text='구매/상담'
-        onClick={() => console.log('구매/상담')}
+        onClick={() =>
+          window.open(
+            'https://www.hyundai.com/kr/ko/e/vehicles/purchase-consult',
+            '_blank',
+          )
+        }
       />
     </div>
   );
