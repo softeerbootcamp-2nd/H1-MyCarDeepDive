d639510a8b58eb990210182517ec88c1bcc19cb6
NamSeok-Bae <qoskatjr2@gmail.com>
2023-08-24 23:20:06 +0900

    chore: ColorChangeType Case 변경

--- GetYa/GetYa/Util/Enum/ColorChangeType.swift
+++ GetYa/GetYa/Util/Enum/ColorChangeType.swift
@@ -8,9 +8,10 @@
 import Foundation
 
 enum ColorChangeType {
-    case canChange
-    case needChnageExteriorColor
-    case needChnageExteriorColorWithTrim
-    case needChnageInteriorColor
-    case needChnageInteriorColorWithTrim
+    case canChange // 색상 선택 가능
+    case canChaneWithTrim // 색상 선택 가능하지만 트림 변경
+    case needChnageExteriorColor // 외장 색상의 변경이 필요함 (채택률 높은걸로)
+    case needChnageExteriorColorWithTrim // 트림과 외장 색상의 변경이 필요함
+    case needChnageInteriorColor // 내장 색상의 변경이 필요함 (채택률 높은걸로)
+    case needChnageInteriorColorWithTrim // 트림과 내장 색상의 변경이 필요함
 }
