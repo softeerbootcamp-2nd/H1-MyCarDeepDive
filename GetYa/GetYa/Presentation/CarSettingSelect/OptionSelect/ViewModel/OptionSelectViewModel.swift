bca78cd7855ddc36bc56a6702abcf4ad32403b3f
NamSeok-Bae <qoskatjr2@gmail.com>
2023-08-27 17:06:30 +0900

    feat: OptionSelectViewModel 기본 구현

--- GetYa/GetYa/Presentation/CarSettingSelect/OptionSelect/ViewModel/OptionSelectViewModel.swift
+++ GetYa/GetYa/Presentation/CarSettingSelect/OptionSelect/ViewModel/OptionSelectViewModel.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+import Combine
+
+class OptionSelectViewModel {
+    // MARK: - Input
+    struct Input {
+        
+    }
+    
+    // MARK: - Output
+    struct Output {
+        
+    }
+    
+    // MARK: - Dependency
+    private let useCase: OptionSelectUseCase
+    private var cancellables = Set<AnyCancellable>()
+    
+    // MARK: - Properties
+    
+    // MARK: - LifeCycle
+    init(useCase: OptionSelectUseCase) {
+        self.useCase = useCase
+    }
+    
+    // MARK: - Functions
+    func transform(input: Input) -> Output {
+        let output = Output()
+        
+        return output
+    }
+}
+
