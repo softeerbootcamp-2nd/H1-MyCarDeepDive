2e89f9937847a26eab1699f2ce955f44e53660aa
DaeHee99 <mokday99@gmail.com>
2023-08-28 10:59:02 +0900

    feat: 엔진/바디/구동방식 데이터

--- frontend/src/Pages/TrimSelectionPage/Feature/InformationModal/Item/index.tsx
+++ frontend/src/Pages/TrimSelectionPage/Feature/InformationModal/Item/index.tsx
@@ -5,7 +5,7 @@ interface InformationItemProps {
   image: string;
   name: string;
   description: string;
-  detail: {
+  detail?: {
     key: string;
     value: string;
   }[];
@@ -13,17 +13,21 @@ interface InformationItemProps {
 
 function Item({ image, name, description, detail }: InformationItemProps) {
   return (
-    <div className='mb-5 mx-10 flex gap-8'>
+    <div className='h-[185.13px] mb-5 mx-10 flex gap-8'>
       <img src={image} alt={name} className='w-[320px]' />
       <div className='flex flex-col gap-3 w-[330px]'>
         <span className='font-h1-medium text-grey-100'>{name}</span>
         <span className='font-body4-regular text-grey-100'>{description}</span>
-        <UnderLine margin='' />
-        <div className='flex flex-col gap-1'>
-          {detail.map(item => (
-            <DetailInfo datail={item} key={item.key} />
-          ))}
-        </div>
+        {detail && (
+          <>
+            <UnderLine margin='' />
+            <div className='flex flex-col gap-1'>
+              {detail.map(item => (
+                <DetailInfo datail={item} key={item.key} />
+              ))}
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
