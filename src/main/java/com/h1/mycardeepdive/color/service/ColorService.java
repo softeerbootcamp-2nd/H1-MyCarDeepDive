82c31ee3eee5a614442c444aec4e0eaab42530cf
ajsthfldu <ajsthfldu@outlook.com>
2023-08-28 13:25:47 +0900

    refactor: 조건 분기 메소드로 추출

--- src/main/java/com/h1/mycardeepdive/color/service/ColorService.java
+++ src/main/java/com/h1/mycardeepdive/color/service/ColorService.java
@@ -4,10 +4,14 @@ import static com.h1.mycardeepdive.color.mapper.ColorMapper.toExteriorColorInfo;
 import static com.h1.mycardeepdive.color.mapper.ColorMapper.toInteriorColorInfo;
 
 import com.h1.mycardeepdive.color.domain.*;
-import com.h1.mycardeepdive.color.domain.repository.*;
-import com.h1.mycardeepdive.color.ui.dto.*;
+import com.h1.mycardeepdive.color.domain.repository.TrimExteriorColorRepository;
+import com.h1.mycardeepdive.color.domain.repository.TrimInteriorColorRepository;
+import com.h1.mycardeepdive.color.ui.dto.AllColorResponse;
+import com.h1.mycardeepdive.color.ui.dto.ColorInfo;
+import com.h1.mycardeepdive.color.ui.dto.ColorResponse;
 import com.h1.mycardeepdive.trims.domain.Trim;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.logstash.logback.marker.Markers;
@@ -32,27 +36,29 @@ public class ColorService {
         List<ColorInfo> unavailableColorInfos = new ArrayList<>();
         List<ColorInfo> otherTrimColorInfos = new ArrayList<>();
         List<ExteriorColor> allExteriorColors = trimExteriorColorRepository.findAllExteriorColors();
-        //
+
         List<TrimExteriorColor> trimExteriorColors =
                 trimExteriorColorRepository.findByTrimId(trimId);
         for (TrimExteriorColor trimExteriorColor : trimExteriorColors) {
             ExteriorColor exteriorColor = trimExteriorColor.getExteriorColor();
             Trim trim = trimExteriorColor.getTrim();
-            if (isExteriorColorAvailableForInteriorColor(exteriorColor, interiorColorId)) {
-                availableColors.add(exteriorColor);
-                availableColorInfos.add(toExteriorColorInfo(exteriorColor, trim));
-
-            } else {
-                // todo(else문 최소화)
-                unavailableColors.add(exteriorColor);
-                unavailableColorInfos.add(toExteriorColorInfo(exteriorColor, trim));
-            }
+            separateColors(
+                    interiorColorId,
+                    availableColors,
+                    unavailableColors,
+                    availableColorInfos,
+                    unavailableColorInfos,
+                    exteriorColor,
+                    trim);
         }
         allExteriorColors.removeAll(availableColors);
         allExteriorColors.removeAll(unavailableColors);
         for (ExteriorColor exteriorColor : allExteriorColors) {
-            // todo(매직넘버로 묶기 0)
-            Trim trim = exteriorColor.getTrimExteriorColors().get(0).getTrim();
+            Trim trim =
+                    exteriorColor.getTrimExteriorColors().stream()
+                            .findFirst()
+                            .orElseThrow()
+                            .getTrim();
             otherTrimColorInfos.add(toExteriorColorInfo(exteriorColor, trim));
         }
         availableColorInfos.sort((o1, o2) -> (int) (o2.getChoose_rate() - o1.getChoose_rate()));
@@ -68,26 +74,29 @@ public class ColorService {
         List<ColorInfo> unavailableColorInfos = new ArrayList<>();
         List<ColorInfo> otherTrimColorInfos = new ArrayList<>();
         List<InteriorColor> allInteriorColors = trimInteriorColorRepository.findAllInteriorColors();
-        //
+
         List<TrimInteriorColor> trimInteriorColors =
                 trimInteriorColorRepository.findByTrimId(trimId);
         for (TrimInteriorColor trimInteriorColor : trimInteriorColors) {
             InteriorColor interiorColor = trimInteriorColor.getInteriorColor();
             Trim trim = trimInteriorColor.getTrim();
-            if (isInteriorColorAvailableForExteriorColor(interiorColor, exteriorColorId)) {
-                availableColors.add(interiorColor);
-                availableColorInfos.add(toInteriorColorInfo(interiorColor, trim));
-            } else {
-                // todo(else문 최소화)
-                unavailableColors.add(interiorColor);
-                unavailableColorInfos.add(toInteriorColorInfo(interiorColor, trim));
-            }
+            separateColors(
+                    exteriorColorId,
+                    availableColors,
+                    unavailableColors,
+                    availableColorInfos,
+                    unavailableColorInfos,
+                    interiorColor,
+                    trim);
         }
         allInteriorColors.removeAll(availableColors);
         allInteriorColors.removeAll(unavailableColors);
         for (InteriorColor interiorColor : allInteriorColors) {
-            // todo(매직넘버로 묶기 0)
-            Trim trim = interiorColor.getTrimInteriorColors().get(0).getTrim();
+            Trim trim =
+                    interiorColor.getTrimInteriorColors().stream()
+                            .findFirst()
+                            .orElseThrow()
+                            .getTrim();
             otherTrimColorInfos.add(toInteriorColorInfo(interiorColor, trim));
         }
         availableColorInfos.sort((o1, o2) -> (int) (o2.getChoose_rate() - o1.getChoose_rate()));
@@ -105,18 +114,27 @@ public class ColorService {
                         .findFirst()
                         .orElseThrow();
         ColorResponse interiorColorResponse = findInteriorColors(trimId, exteriorColor.getId());
-        // todo(매직넘버로 묶기 0)
-        ColorInfo interiorColorInfo = interiorColorResponse.getAvailable_colors().get(0);
+        ColorInfo interiorColorInfo =
+                interiorColorResponse.getAvailable_colors().stream().findFirst().orElseThrow();
         Long interiorId = interiorColorInfo.getColor_id();
         ColorResponse colorResponse = findExteriorColors(trimId, interiorId);
         return new AllColorResponse(colorResponse, interiorColorResponse);
+    }
+
+    public boolean userClickedExteriorColorLog(Long exteriorColorId) {
+        log.info(Markers.append(EXTERIOR_COLOR, exteriorColorId), EXTERIOR_COLOR);
+        return true;
+    }
+
+    public boolean userClickedInteriorColorLog(Long interiorColorId) {
+        log.info(Markers.append(INTERIOR_COLOR, interiorColorId), INTERIOR_COLOR);
+        return true;
     }
 
     private boolean isExteriorColorAvailableForInteriorColor(
             ExteriorColor exteriorColor, Long interiorColorId) {
         List<ColorCombination> colorCombinations = exteriorColor.getColorCombinations();
         for (ColorCombination colorCombination : colorCombinations) {
-            // todo(선택): 삼항연산자?
             if (colorCombination.getInteriorColor().getId().equals(interiorColorId)) {
                 return true;
             }
@@ -135,13 +153,37 @@ public class ColorService {
         return false;
     }
 
-    public boolean userClickedExteriorColorLog(Long exteriorColorId) {
-        log.info(Markers.append(EXTERIOR_COLOR, exteriorColorId), EXTERIOR_COLOR);
-        return true;
+    private void separateColors(
+            Long interiorColorId,
+            List<ExteriorColor> availableColors,
+            List<ExteriorColor> unavailableColors,
+            List<ColorInfo> availableColorInfos,
+            List<ColorInfo> unavailableColorInfos,
+            ExteriorColor exteriorColor,
+            Trim trim) {
+        if (isExteriorColorAvailableForInteriorColor(exteriorColor, interiorColorId)) {
+            availableColors.add(exteriorColor);
+            availableColorInfos.add(toExteriorColorInfo(exteriorColor, trim));
+            return;
+        }
+        unavailableColors.add(exteriorColor);
+        unavailableColorInfos.add(toExteriorColorInfo(exteriorColor, trim));
     }
 
-    public boolean userClickedInteriorColorLog(Long interiorColorId) {
-        log.info(Markers.append(INTERIOR_COLOR, interiorColorId), INTERIOR_COLOR);
-        return true;
+    private void separateColors(
+            Long exteriorColorId,
+            List<InteriorColor> availableColors,
+            List<InteriorColor> unavailableColors,
+            List<ColorInfo> availableColorInfos,
+            List<ColorInfo> unavailableColorInfos,
+            InteriorColor interiorColor,
+            Trim trim) {
+        if (isInteriorColorAvailableForExteriorColor(interiorColor, exteriorColorId)) {
+            availableColors.add(interiorColor);
+            availableColorInfos.add(toInteriorColorInfo(interiorColor, trim));
+            return;
+        }
+        unavailableColors.add(interiorColor);
+        unavailableColorInfos.add(toInteriorColorInfo(interiorColor, trim));
     }
 }
